2b1dbd49b245804e79ebde0b6f7a796fb88b4ace
zhangyingtao75144 <125929019@qq.com>
2017-03-22 13:35:15 +0800

    init

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('SAVOR_M_TP_PATH' , __DIR__);
 define('APP_DEBUG',True);
 // 定义应用目录
-print_r($_SERVER);exit;
 define('APP_PATH', SAVOR_M_TP_PATH . '/Application/');
 define('APP_STATUS','config_test');
 

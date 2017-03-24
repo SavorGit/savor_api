f15552a67c294196099b137de7a394fb81e5eae1
zhangyingtao75144 <125929019@qq.com>
2017-03-24 14:09:52 +0800

    修改分类接口数据返回不能为null

--- Application/Basedata/Controller/CategoryController.class.php
+++ Application/Basedata/Controller/CategoryController.class.php
@@ -20,12 +20,14 @@ class CategoryController extends BaseController{
     public function getCategoryList(){
 
         $m_category = new \Common\Model\Basedata\CategoryModel();
-      //  var_dump($this->params);
-        //var_dump($this->traceinfo);
-        //var_dump($_SERVER);
-
         $data = $m_category->getAllCategory();
-        
+        foreach($data as $key=>$v){
+           foreach($v as $kk=>$vv){
+               if(empty($vv)){
+                   unset($data[$key][$kk]);
+               }
+           }
+        }
         $this->to_back($data);
     }
 }

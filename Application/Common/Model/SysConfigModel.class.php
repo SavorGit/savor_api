4bf3b15e5330921352100629e02f6c5a73c24c7f
zhangyingtao75144 <125929019@qq.com>
2017-05-09 13:39:44 +0800

    修改获取数据条件

--- Application/Common/Model/SysConfigModel.class.php
+++ Application/Common/Model/SysConfigModel.class.php
@@ -6,7 +6,7 @@ class SysConfigModel extends Model{
 	protected $tableName = 'sys_config';
 	public function getInfo($where){
 	    if($where){
-	        $where =" config_key in(".$where.")";
+	        $where =" config_key in(".$where.") and status=1";
 	    }
 	    $result = $this->where($where)->select();
 	    return $result;

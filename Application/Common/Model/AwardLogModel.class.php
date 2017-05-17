760e8282a419727d193d1769bcc8bc6c20fcbd4f
zhangyingtao75144 <125929019@qq.com>
2017-05-17 10:40:23 +0800

    修改抽奖次数统计

--- Application/Common/Model/AwardLogModel.class.php
+++ Application/Common/Model/AwardLogModel.class.php
@@ -12,8 +12,11 @@ class AwardLogModel extends Model
 	protected $tableName='award_log';
 	public function countAwardLog($deviceid,$date){
 	    $map['deviceid'] = $deviceid;
-	    $map['date_time'] = $date; 
-	    return $this->where($map)->count();
+	    $start_time = $date.' 00:00:00';
+	    $end_time = $date.' 23:59:59';
+	    $sql ="select count(id) as count  from `savor_award_log` where `deviceid`='".$map['deviceid']."' and `time`>='".$start_time."' and time<='".$end_time."'"; 
+	    $ret = $this->query($sql);
+	    return $ret[0]['count'];
 	}
 	public function addInfo($data){
 	    return $this->add($data);

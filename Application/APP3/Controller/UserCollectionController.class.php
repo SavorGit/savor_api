c658d2fb8246e129ab92c0d8cb29ea8ebe34046e
xiaobaig0407 <xiaobaig0407@163.com>
2017-07-10 13:32:29 +0800

    修改

--- Application/APP3/Controller/UserCollectionController.class.php
+++ Application/APP3/Controller/UserCollectionController.class.php
@@ -12,11 +12,14 @@ class UserCollectionController extends BaseController{
             case 'addMyCollection':
                 $this->is_verify = 1;
                 break;
+            case 'getLastCollectoinList':
+                $this->is_verify = 0;
+                break;
         }
         parent::_init_();
     }
     /**
-     * @desc 获取分类列表
+     * @desc 收藏取肖收藏
      */
     public function addMyCollection(){
         $d_time = date("Y-m-d H:i:s");
@@ -59,6 +62,68 @@ class UserCollectionController extends BaseController{
 
             }
         }
+        $this->to_back($data);
+    }
+
+
+    /**
+     * @desc 下拉二十条
+     */
+    public function getLastCollectoinList(){
+        $usecModel = new \Common\Model\UserCollectionModel();
+        $traceinfo = $this->traceinfo;
+        $deviceid = $traceinfo['deviceid'];
+        if(empty($save['device_id'])){
+            $data = 18001;
+        }
+        $result = $usecModel->getCollecitonList($deviceid, '',1);
+        $data = array();
+        //判断结果
+        foreach($result as $key=>$v){
+            foreach($v as $kk=> $vv){
+                if(empty($vv)){
+                    unset($result[$key][$kk]);
+                }
+            }
+            $result[$key]['imageURL'] = $this->getOssAddr($v['imgUrl']) ;
+            $result[$key]['contentURL'] = $this->getContentUrl($v['contentUrl']);
+            if(!empty($v['videoUrl'])) $result[$key]['videoURL']   = substr($v['videoUrl'],0,strpos($v['videoUrl'], '.f')) ;
+            if($v['type'] ==3){
+                if(empty($v['name'])){
+                    unset($result[$key]['name']);
+                }else{
+                    $ttp = explode('/', $v['name']);
+                    $result[$key]['name'] = $ttp[2];
+                }
+            }
+            if($v['type'] ==3 && empty($v['content'])){
+                $result[$key]['type'] = 4;
+            }
+            $result[$key]['createTime'] = strtotime($v['createTime']);
+
+            /* if(!empty($createTime)){
+                $str_create_time = strtotime($v['createTime']);
+
+            } */
+            $ids[] = $v['id'];
+            unset($result[$key]['content'],$result[$key]['contentUrl'],$result[$key]['videoUrl'],$result[$key]['imgUrl']);
+        }
+        if($result){
+            $num = count($result) -1 ;
+            $data['list'] = $result;
+            $data['time'] = $result[$num]['sort_num'];
+            $data['minTime'] = $result[0]['createTime'];
+
+            $data['maxTime'] = $result[$num]['sort_num'];
+            if(!empty($flag)){
+                $old_ids = explode(',', $flag);
+                $update_info = array_diff($ids, $old_ids);
+                $data['count'] = count($update_info);
+
+            }
+            $data['flag'] = implode(',', $ids);
+        }
+
         $this->to_back($data);
     }
 }

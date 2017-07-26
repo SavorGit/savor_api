3627407fff1d53eccdc804f485de9c04434ddcef
xiaobaig0407 <xiaobaig0407@163.com>
2017-07-26 16:01:58 +0800

    FIX 无法显示上海

--- Application/Common/Model/HotelModel.class.php
+++ Application/Common/Model/HotelModel.class.php
@@ -36,7 +36,7 @@ class HotelModel extends Model
         }
     }
     public function getHotelInfoByMac($mac){
-        $sql ="select he.mac_addr,h.name as hotel_name,a.id as area_id,a.name as area_name
+        $sql ="select he.mac_addr,h.name as hotel_name,a.id as area_id,a.region_name as area_name
                from savor_hotel as h
                left join savor_hotel_ext as he on h.id=he.hotel_id
                left join savor_area_info as a on h.area_id =a.id where h.flag=0 and he.mac_addr='".$mac."'";

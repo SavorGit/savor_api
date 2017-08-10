cb781c9da4c85c510a4584dfc126eb9b7084f582
zhangyingtao75144 <125929019@qq.com>
2017-08-10 09:43:15 +0800

    心跳上报机顶盒获取对应的城市id

--- Application/Common/Model/BoxModel.class.php
+++ Application/Common/Model/BoxModel.class.php
@@ -13,11 +13,11 @@ class BoxModel extends Model
     public function getHotelInfoByBoxMac($mac){
         if($mac){
             $sql ="select b.id as box_id,b.name as box_name,b.room_id,r.name as room_name, h.id as hotel_id,
-                   h.name as hotel_name,a.id as area_id, a.name as area_name
+                   h.name as hotel_name,a.id as area_id, a.region_name as area_name
                    from savor_box as b
                    left join savor_room as r on b.room_id=r.id
                    left join savor_hotel as h on r.hotel_id=h.id
-                   left join savor_area as a on h.area_id=a.id 
+                   left join savor_area_info as a on h.area_id=a.id 
                    where b.flag=0 and  b.mac='".$mac."' limit 1";
             $result = $this->query($sql);
             if($result){

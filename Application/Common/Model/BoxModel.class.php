90ec22a952290588a8eed7f795c7142af5a288ee
xiaobaig0407 <xiaobaig0407@163.com>
2017-09-11 19:25:26 +0800

    只显示非冻结的

--- Application/Common/Model/BoxModel.class.php
+++ Application/Common/Model/BoxModel.class.php
@@ -18,7 +18,7 @@ class BoxModel extends Model
                    left join savor_room as r on b.room_id=r.id
                    left join savor_hotel as h on r.hotel_id=h.id
                    left join savor_area_info as a on h.area_id=a.id 
-                   where b.flag=0 and  b.mac='".$mac."' limit 1";
+                   where b.flag=0 and b.state!=2 and r.flag=0 and r.state!=2 and b.mac='".$mac."' limit 1";
             $result = $this->query($sql);
             if($result){
                 return $result[0];

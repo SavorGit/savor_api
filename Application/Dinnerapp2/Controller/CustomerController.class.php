963b211f6e48883d7b4ca40910cee0d691d5e4bf
xiaobaig0407 <xiaobaig0407@163.com>
2017-12-19 20:14:35 +0800

    添加餐厅端第2版防止冲突

---
+++ Application/Dinnerapp2/Controller/CustomerController.class.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @desc 客户信息管理
+ * @author baiyutao
+ * @date  20171219
+ */
+namespace Dinnerapp\Controller;
+use Think\Controller;
+use \Common\Controller\BaseController as BaseController;
+class CustomerController extends BaseController{
+    /**
+     * 构造函数
+     */
+    function _init_() {
+        switch(ACTION_NAME) {
+            case 'addCustom':
+                $this->is_verify = 1;
+                $this->valid_fields = array(
+                    'invite_id' =>1001,
+                    'mobile'    =>1001,
+                    'name'      =>1001,
+                );
+                break;
+            default:
+                 break;
+        }
+        parent::_init_();
+        $this->vcode_valid_time =  600;
+    }
+
+    public function addCustom() {
+        $mobile = $this->params['mobile'];
+        //验证手机格式
+        if(!check_mobile($mobile)){
+            $this->to_back(60002);
+        }
+        $invite_code = $this->params['invite_id'];
+        if(!is_numeric($invite_code)) {
+            $this->to_back(60100);
+        }
+        //判断用户名是否存在
+        
+        $username    = $this->params['name'];
+        $username    = $this->params['name'];
+        $username    = $this->params['name'];
+        $username    = $this->params['name'];
+        $username    = $this->params['name'];
+        $username    = $this->params['name'];
+        $username    = $this->params['name'];
+    }
+
+    
+}

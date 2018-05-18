dbae51dd413ed4821183067a552b51e92dabf971
zhangyingtao75144 <125929019@qq.com>
2018-05-18 18:31:33 +0800

    去除手机号收尾的空格

--- Application/Opclient11/Controller/TaskController.class.php
+++ Application/Opclient11/Controller/TaskController.class.php
@@ -126,6 +126,7 @@ class TaskController extends BaseController{
         $task_id = $m_option_task->addData($data, $type=1); 
         
         if($task_type==4 || $task_type ==2){//如果是维修 或者是安装验收
+            $mobile = trim($mobile);
             if(empty($mobile)){
                 $this->to_back('30074');
             }else  if(!check_mobile($mobile)){

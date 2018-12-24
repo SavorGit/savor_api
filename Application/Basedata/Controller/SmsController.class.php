c13539e136b8f0eab3dcbd7e11f6e5d5e34a85f0
liubin0125 <liubin-0125@163.com>
2018-12-24 14:46:51 +0800

    新增发送短信接口

---
+++ Application/Basedata/Controller/SmsController.class.php
@@ -0,0 +1,50 @@
+<?php
+namespace BaseData\Controller;
+use \Common\Controller\CommonController as CommonController;
+class SmsController extends CommonController{
+
+    /**
+     * 构造函数
+     */
+    function _init_() {
+        switch(ACTION_NAME) {
+            case 'sendSms':
+                $this->is_verify = 0;
+                break;
+        }
+        parent::_init_();
+    }
+
+    private function sendSms(){
+        $tel = I('tel',0,'intval');
+        $templateId = I('templateid',0,'intval');
+
+        if(empty($tel) || empty($templateId)){
+            $this->to_back(1001);
+        }
+        $param = null;
+
+        $ucconfig = C('SMS_CONFIG');
+        $options = array('accountsid'=>$ucconfig['accountsid'],'token'=>$ucconfig['token']);
+        $ucpass= new \Common\Lib\Ucpaas($options);
+        $appId = $ucconfig['appid'];
+        $res_json = $ucpass->templateSMS($appId,$tel,$templateId,$param);
+        $res_data = json_decode($res_json,true);
+        if(is_array($res_data) && $res_data['resp']['respCode']=== '000000') {
+            $data = array();
+            $data['type'] = 7;//检测netty
+            $data['status'] = 1;
+            $data['create_time'] = date('Y-m-d H:i:s');
+            $data['update_time'] = date('Y-m-d H:i:s');
+            $data['url'] = '';
+            $data['tel'] = $tel;
+            $data['resp_code'] = $res_data['resp']['respCode'];
+            $data['msg_type'] = 3;//通知类型
+            $m_account_sms_log =  new \Common\Model\AccountMsgLogModel();
+            $m_account_sms_log->addData($data);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

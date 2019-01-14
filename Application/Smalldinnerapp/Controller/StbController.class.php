7413d06fa6fa37de6d6363ec865c5aae787726c7
liubin0125 <liubin-0125@163.com>
2019-01-14 18:13:18 +0800

    修改获取盒子列表

--- Application/Smalldinnerapp/Controller/StbController.class.php
+++ Application/Smalldinnerapp/Controller/StbController.class.php
@@ -23,8 +23,17 @@ class StbController extends CommonController{
         $hotel_id = intval($this->params['hotel_id']);
         $fields = 'c.id as room_id,c.name as room_name,a.name as box_name,a.mac as box_mac';
         $m_box = new \Common\Model\BoxModel();
-        $box_list = $m_box->getBoxListByHotelid($fields,$hotel_id);
-        $this->to_back($box_list);
+        $res_box = $m_box->getBoxListByHotelid($fields,$hotel_id);
+        $box_list = $box_name_list = array();
+        if(!empty($res_box)) {
+            array_unshift($res_box,array('box_name'=>'请选择包间电视','box_mac'=>''));
+            foreach ($res_box as $k=>$v) {
+                $box_list[] = array('id'=>$k,'name'=>$v['box_name'],'box_mac'=>$v['box_mac']);
+                $box_name_list[] = $v['box_name'];
+            }
+        }
+        $res_data = array('box_list'=>$box_list,'box_name_list'=>$box_name_list);
+        $this->to_back($res_data);
     }
     
 }

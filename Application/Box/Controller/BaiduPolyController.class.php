c08df634dcbb5d142c47e3ccc17ae9b4d1b5359c
zhangyingtao75144 <125929019@qq.com>
2019-02-15 09:33:31 +0800

    新增奥铃聚屏广告

--- Application/Box/Controller/BaiduPolyController.class.php
+++ Application/Box/Controller/BaiduPolyController.class.php
@@ -11,7 +11,8 @@ class BaiduPolyController extends CommonController{
         switch(ACTION_NAME) {
             case 'recordPlay':
                 $this->is_verify = 1;
-                $this->valid_fields = array('box_mac'=>1001,'media_id'=>1001);
+                $this->valid_fields = array('box_mac'=>1001,'media_id'=>1000,
+                                            'media_name'=>1000,'media_md5'=>1000,'tpmedia_id'=>1000);
                 break;    
         }
         parent::_init_(); 
@@ -20,13 +21,29 @@ class BaiduPolyController extends CommonController{
      * @desc 记录百度聚屏广告播放
      */
     public function recordPlay(){
-        $box_mac = $this->params['box_mac'];      //机顶盒mac
-        $media_id = $this->params['media_id'];    //广告资源id
+        $box_mac   = $this->params['box_mac'];      //机顶盒mac
+        $media_id  = $this->params['media_id'] ? $this->params['media_id'] :0;     //广告资源id
+        $media_name= $this->params['media_name'];   //广告资源名称
+        $media_md5 = $this->params['media_md5'];    //广告资源md5
+        $tpmedia_id= $this->params['tpmedia_id'];   //第三方媒体 1:百度 2:钛镁 3:奥凌
         $redis =  SavorRedis::getInstance();
         $redis->select(4);
         $times = getMillisecond();
         $cache_key = $box_mac.":".$times;
-        $redis->set($cache_key, $media_id);
+        
+        $data = array();
+        $data['media_id']  = $media_id;
+        $data['media_name']= $media_name;
+        $data['media_md5'] = $media_md5;
+        $data['tpmedia_id']= $tpmedia_id;
+        $redis->set($cache_key, json_encode($data));
+        
+        /* if(!empty($media_id)){
+            $redis->set($cache_key, $media_id);
+        }else {
+            
+        } */
+        
         $this->to_back(10000);
         
     }

24a0262bd7b45b4a9da8d84de1664884a90805f3
liubin0125 <liubin-0125@163.com>
2019-04-16 16:05:02 +0800

    新增获取广告位接口

---
+++ Application/Smallapp3/Controller/AdspositionController.class.php
@@ -0,0 +1,44 @@
+<?php
+namespace Smallapp3\Controller;
+use \Common\Controller\CommonController;
+class AdspositionController extends CommonController{
+    /**
+     * 构造函数
+     */
+    function _init_() {
+        switch(ACTION_NAME) {
+            case 'getAdspositionList':
+                $this->is_verify = 1;
+                $this->valid_fields = array('position'=>1001);
+                break;
+        }
+        parent::_init_();
+    }
+
+    /**
+     * @desc 获取广告位列表
+     */
+    public function getAdspositionList(){
+        $position = intval($this->params['position']);
+        $fields = 'id,name,media_id,linkcontent,clicktype,appid,position';
+        $where = array('status'=>1);
+        if($position){
+            $where['position'] = $position;
+        }
+        $orderby = 'id desc';
+        $m_adsposition = new \Common\Model\Smallapp\AdspositionModel();
+        $res_positions = $m_adsposition->getDataList($fields,$where,$orderby);
+
+        $result = array();
+        if(!empty($res_positions)){
+            $m_media = new \Common\Model\MediaModel();
+            foreach ($res_positions as $k=>$v){
+                unset($v['media_id']);
+                $res_media = $m_media->getMediaInfoById($v['media_id']);
+                $v['oss_addr'] = $res_media['oss_addr'];
+                $result[] = $v;
+            }
+        }
+        $this->to_back($result);
+    }
+}

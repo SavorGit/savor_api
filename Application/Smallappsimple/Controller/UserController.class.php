e74ac2fe35477a46cffecf8ec9c3f641d5178d93
zhangyingtao75144 <125929019@qq.com>
2019-04-24 15:07:16 +0800

    热点投屏极简版授权登录获取unionid

--- Application/Smallappsimple/Controller/UserController.class.php
+++ Application/Smallappsimple/Controller/UserController.class.php
@@ -21,6 +21,14 @@ class UserController extends CommonController{
                 $this->is_verify =1;
                 $this->valid_fields = array('openid'=>1001);
                 break;
+            case 'registerCom':
+                $this->is_verify = 1;
+                $this->valid_fields = array('openid'=>1001,'avatarUrl'=>1000,
+                    'nickName'=>1000,'gender'=>1000,
+                    'session_key'=>1001,'iv'=>1001,
+                    'encryptedData'=>1001,
+                );
+                break;
         }
         parent::_init_();
     }
@@ -84,6 +92,36 @@ class UserController extends CommonController{
             $data['nickName']  = $this->params['nickName'];
             $data['gender']    = $this->params['gender'];
             $data['is_wx_auth']= 2;
+            $data['small_app_id'] = 3;
+            $m_user->updateInfo($where, $data);
+            $data['openid'] = $openid;
+            $this->to_back($data);
+        }
+    }
+    public function registerCom(){
+        $openid = $this->params['openid'];
+        $m_user = new \Common\Model\Smallapp\UserModel();
+        $where = array();
+        $where['openid'] = $openid;
+        $nums = $m_user->countNum($where);
+        $encryptedData = $this->params['encryptedData'];
+        if(empty($nums)){
+            $data['openid']    = $openid;
+            $data['avatarUrl'] = $this->params['avatarUrl'];
+            $data['nickName']  = $this->params['nickName'];
+            $data['gender']    = $this->params['gender'];
+            $data['unionId']   = $encryptedData['unionId'];
+            $data['is_wx_auth']= 3;
+            $data['small_app_id'] = 3;
+            $m_user->addInfo($data);
+            $this->to_back($data);
+        }else {
+            $data = array();
+            $data['avatarUrl'] = $this->params['avatarUrl'];
+            $data['nickName']  = $this->params['nickName'];
+            $data['gender']    = $this->params['gender'];
+            $data['unionId']   = $encryptedData['unionId'];
+            $data['is_wx_auth']= 3;
             $data['small_app_id'] = 3;
             $m_user->updateInfo($where, $data);
             $data['openid'] = $openid;

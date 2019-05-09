adb78fdd30f6af552cb9f483b253767d17da3ec3
liubin0125 <liubin-0125@163.com>
2019-05-09 15:56:08 +0800

    新增红包黑名单

---
+++ Application/Common/Model/Smallapp/ForscreenInvalidlistModel.class.php
@@ -0,0 +1,7 @@
+<?php
+namespace Common\Model\Smallapp;
+use Common\Model\BaseModel;
+
+class ForscreenInvalidlistModel extends BaseModel{
+	protected $tableName='smallapp_forscreen_invalidlist';
+}

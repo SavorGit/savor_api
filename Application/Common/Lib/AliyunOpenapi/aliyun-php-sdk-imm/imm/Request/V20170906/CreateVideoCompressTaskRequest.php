9d6396883d0493928eba60921b5f15abedd1b368
liubin0125 <liubin-0125@163.com>
2019-07-01 14:56:33 +0800

    新增阿里云智能媒体管理SDK

---
+++ Application/Common/Lib/AliyunOpenapi/aliyun-php-sdk-imm/imm/Request/V20170906/CreateVideoCompressTaskRequest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace imm\Request\V20170906;
+
+/**
+ * @deprecated Please use https://github.com/aliyun/openapi-sdk-php
+ *
+ * Request of CreateVideoCompressTask
+ *
+ * @method string getVideoUri()
+ * @method string getNotifyTopicName()
+ * @method string getTargetList()
+ * @method string getNotifyEndpoint()
+ * @method string getProject()
+ */
+class CreateVideoCompressTaskRequest extends \RpcAcsRequest
+{
+
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'imm',
+            '2017-09-06',
+            'CreateVideoCompressTask',
+            'imm'
+        );
+    }
+
+    /**
+     * @param string $videoUri
+     *
+     * @return $this
+     */
+    public function setVideoUri($videoUri)
+    {
+        $this->requestParameters['VideoUri'] = $videoUri;
+        $this->queryParameters['VideoUri'] = $videoUri;
+
+        return $this;
+    }
+
+    /**
+     * @param string $notifyTopicName
+     *
+     * @return $this
+     */
+    public function setNotifyTopicName($notifyTopicName)
+    {
+        $this->requestParameters['NotifyTopicName'] = $notifyTopicName;
+        $this->queryParameters['NotifyTopicName'] = $notifyTopicName;
+
+        return $this;
+    }
+
+    /**
+     * @param string $targetList
+     *
+     * @return $this
+     */
+    public function setTargetList($targetList)
+    {
+        $this->requestParameters['TargetList'] = $targetList;
+        $this->queryParameters['TargetList'] = $targetList;
+
+        return $this;
+    }
+
+    /**
+     * @param string $notifyEndpoint
+     *
+     * @return $this
+     */
+    public function setNotifyEndpoint($notifyEndpoint)
+    {
+        $this->requestParameters['NotifyEndpoint'] = $notifyEndpoint;
+        $this->queryParameters['NotifyEndpoint'] = $notifyEndpoint;
+
+        return $this;
+    }
+
+    /**
+     * @param string $project
+     *
+     * @return $this
+     */
+    public function setProject($project)
+    {
+        $this->requestParameters['Project'] = $project;
+        $this->queryParameters['Project'] = $project;
+
+        return $this;
+    }
+}

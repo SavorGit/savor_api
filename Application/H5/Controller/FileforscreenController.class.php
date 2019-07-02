a1076e23cdbfc18c714c0f6f2fd7eb7f5dd4adb7
liubin0125 <liubin-0125@163.com>
2019-07-02 16:35:59 +0800

    限制文件上传类型

--- Application/H5/Controller/FileforscreenController.class.php
+++ Application/H5/Controller/FileforscreenController.class.php
@@ -8,6 +8,8 @@ use Think\Controller;
 class FileforscreenController extends Controller {
 
     public function index(){
+        $file_ext = C('SAPP_FILE_FORSCREEN_TYPES');
+        $this->assign('file_ext',join(',',$file_ext));
         $this->display();
     }
 

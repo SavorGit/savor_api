8ff7d5eccfbb065ee29bacd0036f8be131298644
liubin0125 <liubin-0125@163.com>
2019-07-02 16:09:45 +0800

    新增阿里云OSS上传web

---
+++ Application/H5/Controller/FileforscreenController.class.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * H5文件投屏
+ */
+namespace H5\Controller;
+use Think\Controller;
+
+class FileforscreenController extends Controller {
+
+    public function index(){
+        $this->display();
+    }
+
+}

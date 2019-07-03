038de11574cf9608b999a6d03880f984ee2aa19d
liubin0125 <liubin-0125@163.com>
2019-07-03 14:33:02 +0800

    新增文件投屏缓存key

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -241,6 +241,7 @@ return array(
     'SAPP_UPRES_FORSCREEN'=>'smallapp:upresouce',
     'SAPP_UPDOWN_FORSCREEN'=>'smallapp:boxupdown:',
     'SAPP_QRCODE'=>'smallapp:qrcode:',
+    'SAPP_FILE_FORSCREEN'=>'smallapp:fileforscreen',
     'SAPP_WANT_GAME'      =>'smallapp:wantgame', //想要点开小程序互动游戏
     'SAPP_PLAY_GAME'      =>'smallapp:playgame',
     'SAPP_SUNCODE_LOG'    =>'smallapp:suncodelog:', //电视显示小程序码时长日志

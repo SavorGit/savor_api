9440cf384e1bce18a2dba789d5e7d200039d779e
liubin0125 <liubin-0125@163.com>
2019-07-19 17:51:02 +0800

    新增商品列表

---
+++ Application/Smalldinnerapp11/Controller/GoodsController.class.php
@@ -0,0 +1,38 @@
+<?php
+namespace Smalldinnerapp11\Controller;
+use \Common\Controller\CommonController as CommonController;
+
+class GoodsController extends CommonController{
+    /**
+     * 构造函数
+     */
+    function _init_() {
+        switch(ACTION_NAME) {
+            case 'getGoodslist':
+                $this->is_verify = 1;
+                $this->valid_fields = array('offset'=>1001,'pagesize'=>1001,'type'=>1001,'hotel_id'=>1001);
+        }
+        parent::_init_();
+    }
+
+    public function getGoodslist(){
+        $offset = intval($this->params['offset']);
+        $pagesize = intval($this->params['pagesize']);
+        $type = $this->params['type'];//10官方活动促销,20我的活动
+        $m_goods = new \Common\Model\Smallapp\GoodsModel();
+        $fields = 'id as goods_id,name,img_addr,video_addr,price,rebate_integral,jd_url,type';
+        $where = array('type'=>$type);
+        $orderby = 'id desc';
+        $res_goods = $m_goods->getDataList($fields,$where,$orderby,$offset,$pagesize);
+        $datalist = $res_goods['list'];
+
+
+        if ($offset + $pagesize > $res_goods['total']) {
+            $offset = $res_goods['total'];
+        } else {
+            $offset += $pagesize;
+        }
+        $data = array('offset'=>$offset,'pagesize'=>$pagesize,'datalist'=>$datalist,'total'=>$res_goods['total']);
+        $this->to_back($data);
+    }
+}

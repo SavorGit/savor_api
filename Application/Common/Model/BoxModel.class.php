230b77f181e5287259f9dee6e1302ff9f54fcdd6
liubin0125 <liubin-0125@163.com>
2019-07-24 17:25:04 +0800

    新增下单接口

--- Application/Common/Model/BoxModel.class.php
+++ Application/Common/Model/BoxModel.class.php
@@ -131,6 +131,7 @@ class BoxModel extends Model
              ->field($fileds)
              ->join('savor_room c on a.room_id= c.id','left')
              ->join('savor_hotel d on c.hotel_id=d.id','left')
+             ->join('savor_hotel_ext ext on d.id=ext.hotel_id','left')
              ->where($where)
              ->select();
         return $data;

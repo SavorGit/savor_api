a072ba73b918043406d00cd0c20a5dbffe07f7c7
liubin0125 <liubin-0125@163.com>
2019-10-10 09:29:31 +0800

    新增获取酒楼列表

---
+++ Application/Smallsale/Controller/HotelController.class.php
@@ -0,0 +1,36 @@
+<?php
+namespace Smallsale\Controller;
+use \Common\Controller\CommonController as CommonController;
+class HotelController extends CommonController{
+    /**
+     * 构造函数
+     */
+    function _init_() {
+        switch(ACTION_NAME) {
+            case 'getHotelList':
+                $this->is_verify = 0;
+                break;
+        }
+        parent::_init_();
+    }
+
+    public function getHotelList(){
+        $m_hotel = new \Common\Model\HotelModel();
+        $where = array('state'=>1,'flag'=>0);
+        $where['hotel_box_type'] = array('in',array(2,3,6));
+        $res_hotels = $m_hotel->getHotelList($where,'id asc','','id,name');
+
+        $all_hotels = array();
+        foreach ($res_hotels as $v){
+            $letter = getFirstCharter($v['name']);
+            $all_hotels[$letter][]=$v;
+        }
+        ksort($all_hotels);
+        $data = array();
+        foreach ($all_hotels as $k=>$v){
+            $dinfo = array('id'=>ord("$k")-64,'region'=>$k,'items'=>$v);
+            $data[]=$dinfo;
+        }
+        $this->to_back($data);
+    }
+}

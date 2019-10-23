02d7f08a513802af2439783a782512403ac995dc
liubin0125 <liubin-0125@163.com>
2019-10-23 10:37:00 +0800

    优化选择企业税号信息

--- Application/H5/Controller/SaleorderController.class.php
+++ Application/H5/Controller/SaleorderController.class.php
@@ -141,8 +141,12 @@ class SaleorderController extends Controller {
         $m_payment = new \Payment\Model\WxpayModel(3);
         $wxpay = $m_payment->pay($trade_info,$payconfig);
 
+        $hash_ids_key = C('HASH_IDS_KEY');
+        $hashids = new \Common\Lib\Hashids($hash_ids_key);
+        $encode_oid = $hashids->encode($order_id);
+
         $host_name = C('HOST_NAME');
-        $result_url = $host_name.'/h5/saleorder/invoice/oid/'.$order_id;
+        $result_url = $host_name.'/h5/saleorder/invoice/oid/'.$encode_oid;
         $prepay_url = $host_name.'/h5/saleorder/info/oid/'.$order_id;
 
         $this->assign('result_url',$result_url);
@@ -152,13 +156,34 @@ class SaleorderController extends Controller {
     }
 
     public function invoice(){
-        $order_id = I('get.oid',0,'intval');
+        $oid = I('oid','');
+        if(empty($oid)){
+            die('Parameter error');
+        }
+        $hash_ids_key = C('HASH_IDS_KEY');
+        $hashids = new \Common\Lib\Hashids($hash_ids_key);
+        $decode_info = $hashids->decode($oid);
+        if(empty($decode_info)){
+            die('decode error');
+        }
+        $order_id = intval($decode_info[0]);
         $m_order = new \Common\Model\Smallapp\OrderModel();
         $res_order = $m_order->getInfo(array('id'=>$order_id));
         if(empty($res_order)){
             die('order error');
         }
         $oinfo = array('oid'=>$order_id,'total_fee'=>$res_order['total_fee']);
+
+
+        $host_name = C('HOST_NAME');
+        $url = $host_name.'/h5/saleorder/invoice/oid/'.$oid;
+        $m_weixin_api = new \Common\Lib\Weixin_api();
+        $res_config =$m_weixin_api->showShareConfig($url);
+        $this->assign('appid',$res_config['appid']);
+        $this->assign('timestamp',$res_config['timestamp']);
+        $this->assign('noncestr',$res_config['noncestr']);
+        $this->assign('signature',$res_config['signature']);
+
         $this->assign('oinfo',$oinfo);
         $this->display();
     }
@@ -236,6 +261,92 @@ class SaleorderController extends Controller {
         }
         $res = array('code'=>10000,'msg'=>$msg);
         $this->ajaxReturn($res,'JSONP');
+    }
+
+    public function sendaddr(){
+        $order_id = I('oid',0,'intval');
+        $phone = I('phone','','trim');
+
+        if(empty($phone)){
+            $this->ajaxReturn(array('code'=>10001,'msg'=>'请输入手机号码'),'JSONP');
+        }
+        if(!isMobile($phone)){
+            $this->ajaxReturn(array('code'=>10001,'msg'=>'请输入正确的手机号码'),'JSONP');
+        }
+
+        $m_order = new \Common\Model\Smallapp\OrderModel();
+        $res_order = $m_order->getInfo(array('id'=>$order_id));
+        if(empty($res_order)){
+            $this->ajaxReturn(array('code'=>10001,'msg'=>'订单信息有误'),'JSONP');
+        }
+        $order_status = $res_order['status'];//10已下单 11支付失败 12支付成功
+        if($order_status!=12){
+            $this->ajaxReturn(array('code'=>10001,'msg'=>'订单状态错误'),'JSONP');
+        }
+
+        $m_orderinvoice = new \Common\Model\Smallapp\OrderinvoiceModel();
+        $res = $m_orderinvoice->getInfo(array('order_id'=>$order_id));
+        $data = array('order_id'=>$order_id,'phone'=>$phone,'status'=>1);
+        $is_send = 0;
+        if(!empty($res)){
+            switch ($res['status']){//1暂不开票 2已提交开具发票申请 3发票已开
+                case 1:
+                    $m_orderinvoice->updateData(array('id'=>$res['id']),$data);
+                    $is_send = 1;
+                    $msg = '短信已发送,请注意查收';
+                    break;
+                case 2:
+                    $msg = '已提交发票申请,请勿重复提交';
+                    break;
+                case 3:
+                    $msg = '发票已开,请注意查收';
+                    break;
+                default:
+                    $msg = '';
+            }
+        }else{
+            $m_orderinvoice->add($data);
+            $is_send = 1;
+            $msg = '短信已发送,请注意查收';
+        }
+        if($is_send && !empty($res_order['box_mac'])){
+            $ucconfig = C('SMS_CONFIG');
+            $options = array('accountsid'=>$ucconfig['accountsid'],'token'=>$ucconfig['token']);
+            $ucpass= new \Common\Lib\Ucpaas($options);
+            $appId = $ucconfig['appid'];
+
+            $m_box = new \Common\Model\BoxModel();
+            $map = array();
+            $map['a.mac'] = $res_order['box_mac'];
+            $map['a.state'] = 1;
+            $map['a.flag']  = 0;
+            $map['d.state'] = 1;
+            $map['d.flag']  = 0;
+            $fields = 'a.id as box_id,d.name as hotel_name,c.name as room_name';
+            $box_info = $m_box->getBoxInfo($fields, $map);
+            $box_info = $box_info[0];
+
+            $m_goods = new \Common\Model\Smallapp\GoodsModel();
+            $res_goods = $m_goods->getInfo(array('id'=>$res_order['goods_id']));
+
+            $hash_ids_key = C('HASH_IDS_KEY');
+            $hashids = new \Common\Lib\Hashids($hash_ids_key);
+            $encode_oid = $hashids->encode($order_id);
+
+            $param = "{$box_info['hotel_name']},{$res_goods['name']},$encode_oid";
+            $res_json = $ucpass->templateSMS($appId,$phone,$ucconfig['activity_goods_addorder_templateid'],$param);
+            $res_data = json_decode($res_json,true);
+            if(isset($res_data['resp']['respCode'])) {
+                $data = array('type'=>8,'status'=>1,'create_time'=>date('Y-m-d H:i:s'),'update_time'=>date('Y-m-d H:i:s'),
+                    'url'=>$param,'tel'=>$box_info['activity_phone'],'resp_code'=>$res_data['resp']['respCode'],'msg_type'=>3
+                );
+                $m_account_sms_log = new \Common\Model\AccountMsgLogModel();
+                $m_account_sms_log->addData($data);
+            }
+        }
+        $res = array('code'=>10000,'msg'=>$msg);
+        $this->ajaxReturn($res,'JSONP');
+
     }
 
     private function wx_oauth($url){

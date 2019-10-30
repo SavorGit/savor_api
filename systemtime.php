792e78e1580bc9624f95c73a0ce559213f965fd2
zhangyingtao75144 <125929019@qq.com>
2019-10-30 15:07:47 +0800

    test

--- systemtime.php
+++ systemtime.php
@@ -1,3 +1,3 @@
 <?php
  list($t1, $t2) = explode(' ', microtime()); 
- echo  (float)sprintf('%.0f',(floatval($t1)+floatval($t2))*1000); 
+ echo  (float)sprintf('%.0f',(floatval($t1)+floatval($t2))*1000);  

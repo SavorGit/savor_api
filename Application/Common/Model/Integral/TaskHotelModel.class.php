08c10feff7ac4bb9c3c1058ad44aeadd668dfaad
liubin0125 <liubin-0125@163.com>
2019-11-11 09:29:16 +0800

    增加签到领取任务

--- Application/Common/Model/Integral/TaskHotelModel.class.php
+++ Application/Common/Model/Integral/TaskHotelModel.class.php
@@ -4,4 +4,13 @@ use Common\Model\BaseModel;
 
 class TaskHotelModel extends BaseModel{
 	protected $tableName='integral_task_hotel';
+
+	public function getHotelTasks($fileds,$where){
+        $data = $this->alias('a')
+            ->field($fileds)
+            ->join('savor_integral_task task on a.task_id=task.id','left')
+            ->where($where)
+            ->select();
+        return $data;
+    }
 }

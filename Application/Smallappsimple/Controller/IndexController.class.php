3c0006ed5a5a08d73866bac6946f7f44f775ebdf
zhangyingtao75144 <125929019@qq.com>
2019-12-10 15:37:37 +0800

    wifi 错误信息数据上报

--- Application/Smallappsimple/Controller/IndexController.class.php
+++ Application/Smallappsimple/Controller/IndexController.class.php
@@ -318,9 +318,16 @@ class IndexController extends CommonController{
         $box_mac = $this->params['box_mac'];
         $err_info = str_replace('\\', '', $this->params['err_info']);
         $m_err_info = new \Common\Model\Smallapp\WifiErrModel();
-        $data['box_mac'] = $box_mac;
+        $data['box_mac'] = $box_mac !='undefined' ? $box_mac :'';
         $data['err_info'] = $err_info;
-        $m_err_info->addInfo($data);
-        $this->to_back(10000);
+        if($data['box_mac']){
+            $m_err_info->addInfo($data);
+            $this->to_back(10000);
+        }else {
+            $this->to_back(30052);
+            
+        }
+        
+        
     }
 }

5a4a11b3273ebf68fc0d9f65add4f2d123851c48
liubin0125 <liubin-0125@163.com>
2019-12-19 10:59:23 +0800

    增加配置文件

---
+++ Application/Common/Conf/config_test.php
@@ -0,0 +1,75 @@
+<?php
+$DB_MASTER_HOST = '192.168.168.116';
+$DB_SLAVE_HOST  = '192.168.168.116';
+$db_name = 'cloud';
+$db_user = 'phpweb';
+$db_pwd = '123456';
+
+//redis缓存配置
+$redis['db1']['0']['host'] = '192.168.168.116';
+$redis['db1']['0']['port'] = '6380';
+$redis['db1']['0']['password'] = '!1QAZ@2WSX';
+$redis['db1']['0']['isMaster'] = '1';
+$redis['db1']['1']['host'] = '192.168.168.116'; 
+$redis['db1']['1']['port'] = '6380';
+$redis['db1']['1']['password'] = '!1QAZ@2WSX';
+$redis['db1']['1']['isMaster'] = '0';
+
+$config_db =  array(
+	'DB_DEPLOY_TYPE' => 1, //数据库主从支持
+    'DB_RW_SEPARATE' => true, //读写分离
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => "$DB_MASTER_HOST,$DB_SLAVE_HOST",
+    'DB_NAME' => $db_name,
+    'DB_USER' => $db_user,
+    'DB_PWD' => $db_pwd,
+    'DB_PORT' => 3306,
+    'DB_CHARSET' => 'UTF8',
+    'DB_PREFIX' => 'savor_',
+    'DB_DEBUG'  =>  TRUE,
+    'DB_PARAMS' => array(\PDO::ATTR_CASE => \PDO::CASE_NATURAL),
+
+    'DB_OSS'=>array(
+        'DB_DEPLOY_TYPE' => 1, //数据库主从支持
+        'DB_RW_SEPARATE' => true, //读写分离
+        'DB_TYPE' => 'mysql',
+        'DB_HOST' => "$DB_MASTER_HOST,$DB_SLAVE_HOST",
+        'DB_NAME' => 'oss',
+        'DB_USER' => $db_user,
+        'DB_PWD' => $db_pwd,
+        'DB_PORT' => 3306,
+        'DB_CHARSET' => 'UTF8',
+        'DB_PREFIX' => 'oss_',
+        'DB_DEBUG'  =>  TRUE,
+        'DB_PARAMS' => array(\PDO::ATTR_CASE => \PDO::CASE_NATURAL)
+    ),
+    'DB_STATIS'=>array(
+        'DB_DEPLOY_TYPE' => 1, //数据库主从支持
+        'DB_RW_SEPARATE' => true, //读写分离
+        'DB_TYPE' => 'mysql',
+        'DB_HOST' => "$DB_MASTER_HOST,$DB_SLAVE_HOST",
+        'DB_NAME' => 'statisticses',
+        'DB_USER' => $db_user,
+        'DB_PWD' => $db_pwd,
+        'DB_PORT' => 3306,
+        'DB_CHARSET' => 'UTF8',
+        'DB_PREFIX' => 'statistics_',
+        'DB_DEBUG'  =>  TRUE,
+        'DB_PARAMS' => array(\PDO::ATTR_CASE => \PDO::CASE_NATURAL)
+    ),
+
+    'REDIS_CONFIG' => $redis,
+);
+$config_api_host = array(
+'CONTENT_HOST' => 'http://devp.admin.littlehotspot.com/',
+    'IMG_UP_SUBCONTACT' => 'http://devp.oss.littlehotspot.com/log/resource/standalone/mobile',
+    'TASK_REPAIR_IMG' => 'http://devp.oss.littlehotspot.com',
+
+);
+
+return array_merge($config_db,$config_api_host);
+
+
+
+
+

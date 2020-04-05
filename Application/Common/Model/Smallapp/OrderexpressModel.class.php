f79a7d3e8c3e762036d28580ed55533ac4bff611
liubin0125 <liubin-0125@163.com>
2020-04-05 15:00:57 +0800

    优化快递查询方法

--- Application/Common/Model/Smallapp/OrderexpressModel.class.php
+++ Application/Common/Model/Smallapp/OrderexpressModel.class.php
@@ -8,18 +8,19 @@ class OrderexpressModel extends BaseModel{
 	public function getExpress($order_id){
 	    $res_express = $this->getInfo(array('order_id'=>$order_id));
 	    $res_data = array();
-	    if(!empty($res_express)){
+
+        if(!empty($res_express)){
             $express = getExpress($res_express['comcode'],$res_express['enum']);
-            if(!empty($express)){
+            if(!empty($express) && isset($express['returnCode'])){
                 $express = getExpress($res_express['comcode'],$res_express['enum']);
             }
             $all_status = array(
                 '0'=>'在途','1'=>'揽收','2'=>'疑难','3'=>'签收',
                 '4'=>'退签','5'=>'派件','6'=>'退回','7'=>'转投'
             );
-            if(!empty($express)){
-                $express = new \Common\Lib\Express();
-                $all_company = $express->getCompany();
+            if(!empty($express) && !isset($express['returnCode'])){
+                $express_obj = new \Common\Lib\Express();
+                $all_company = $express_obj->getCompany();
                 $res_data = array('enum'=>$express['nu'],'company'=>$all_company[$express['com']]['name'],
                     'state'=>$express['state'],'state_str'=>$all_status[$express['state']]);
                 if(!empty($express['data'])){

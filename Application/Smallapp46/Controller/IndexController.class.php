bdd8109ce39501d3236d88582f8e77094f8956ed
zhangyingtao75144 <125929019@qq.com>
2020-07-09 16:15:48 +0800

    主干版4.6.2 投屏互动日志新增唯一序列号字段

--- Application/Smallapp46/Controller/IndexController.class.php
+++ Application/Smallapp46/Controller/IndexController.class.php
@@ -29,6 +29,17 @@ class IndexController extends CommonController{
                 $this->is_verify = 1;
                 $this->valid_fields = array('openid'=>1001);
                 break;
+            case 'recordForScreenPics':
+                $this->is_verify = 1;
+                $this->valid_fields = array('openid'=>1001,'box_mac'=>1000,
+                    'imgs'=>1001,'mobile_brand'=>1000,
+                    'mobile_model'=>1000,'action'=>1000,
+                    'resource_type'=>1000,'resource_id'=>1000,
+                    'is_pub_hotelinfo'=>1000,'is_share'=>1000,
+                    'forscreen_id'=>1000,'public_text'=>1000,'res_nums'=>1000,
+                    'serial_number'=>1000,
+                );
+                break;
         }
         parent::_init_();
     }
@@ -291,5 +302,142 @@ class IndexController extends CommonController{
         }
         $data['is_comment'] = $is_comment;
         $this->to_back($data);
+    }
+    /**
+     * @desc 记录用户投屏的图片、视频
+     */
+    public function recordForScreenPics(){
+        $forscreen_id = $this->params['forscreen_id'] ? intval($this->params['forscreen_id']) :0;
+        $openid = $this->params['openid'];
+        $box_mac = $this->params['box_mac'];
+        $mobile_brand = $this->params['mobile_brand'];
+        $mobile_model = $this->params['mobile_model'];
+        $forscreen_char = $this->params['forscreen_char'];
+        $public_text   = $this->params['public_text'];
+        $imgs    = str_replace("\\", '', $this->params['imgs']);
+        $action  = $this->params['action'] ? $this->params['action'] : 0;
+        $resource_type = $this->params['resource_type'] ? $this->params['resource_type'] : 0;
+        $resource_id   = $this->params['resource_id'] ? $this->params['resource_id'] : 0;
+        $resource_size = $this->params['resource_size'] ? $this->params['resource_size'] :0;
+        $res_sup_time  = $this->params['res_sup_time'] ? $this->params['res_sup_time'] : 0;
+        $res_eup_time  = $this->params['res_eup_time'] ? $this->params['res_eup_time'] : 0;
+        $is_pub_hotelinfo = $this->params['is_pub_hotelinfo'] ?$this->params['is_pub_hotelinfo']:0;
+        $is_share      = $this->params['is_share'] ? intval($this->params['is_share']) : 0;
+        $duration      = $this->params['duration'] ? $this->params['duration'] : 0.00;
+        $res_nums = $this->params['res_nums']?intval($this->params['res_nums']):0;
+        $serial_number = !empty($this->params['serial_number']) ? $this->params['serial_number'] : '';
+    
+        $data = array();
+        $data['openid'] = $openid;
+        $data['box_mac']= $box_mac;
+        $data['action'] = $action;
+        $data['resource_type'] = $resource_type;
+        $data['resource_id']   = $resource_id;
+        $data['mobile_brand'] = $mobile_brand;
+        $data['mobile_model'] = $mobile_model;
+        $data['imgs']   = $imgs;
+        $data['forscreen_char'] = !empty($forscreen_char) ? $forscreen_char : '';
+        $data['create_time'] = date('Y-m-d H:i:s');
+        $data['res_sup_time']= $res_sup_time;
+        $data['res_eup_time']= $res_eup_time;
+        $data['resource_size'] = $resource_size;
+        $data['is_pub_hotelinfo'] = $is_pub_hotelinfo;
+        $data['is_share']    = $is_share;
+        $data['duration']    = $duration;
+        if($serial_number) $data['$serial_number'] = $serial_number;
+        
+        if($forscreen_id)  $data['forscreen_id'] = $forscreen_id;
+    
+        $redis = SavorRedis::getInstance();
+        $redis->select(5);
+        $history_cache_key = C('SAPP_HISTORY_SCREEN').$box_mac.":".$openid;
+        if($action==4 || ($action==2 && $resource_type==2)) {
+            $redis->rpush($history_cache_key, json_encode($data));
+        }
+    
+        if($is_share){
+            $forscreen_res = json_decode($imgs,true);
+            $oss_addr = $forscreen_res[0];
+            $tempInfo = pathinfo($oss_addr);
+            $surfix = $tempInfo['extension'];
+            if($surfix){
+                $surfix = strtolower($surfix);
+            }
+            $typeinfo = C('RESOURCE_TYPEINFO');
+            if(isset($typeinfo[$surfix])){
+                $type = $typeinfo[$surfix];
+            }else{
+                $type = 3;
+            }
+            $accessKeyId = C('OSS_ACCESS_ID');
+            $accessKeySecret = C('OSS_ACCESS_KEY');
+            $endpoint = 'oss-cn-beijing.aliyuncs.com';
+            $bucket = C('OSS_BUCKET');
+            $aliyunoss = new AliyunOss($accessKeyId, $accessKeySecret, $endpoint);
+            $aliyunoss->setBucket($bucket);
+    
+            if(empty($resource_size) || $resource_size=='undefined'){
+                $this->to_back(90103);
+            }
+            if($type==1){//视频
+                $range = '0-199';
+                $bengin_info = $aliyunoss->getObject($oss_addr,$range);
+                $last_size = $resource_size-1;
+                $last_range = $last_size - 199;
+                $last_range = $last_range.'-'.$last_size;
+                $end_info = $aliyunoss->getObject($oss_addr,$last_range);
+                $file_str = md5($bengin_info).md5($end_info);
+                $fileinfo = strtoupper($file_str);
+            }else{
+                $fileinfo = $aliyunoss->getObject($oss_addr,'');
+            }
+            if(empty($fileinfo)){
+                $this->to_back(90104);
+            }
+            if($fileinfo){
+                $data['md5_file'] = md5($fileinfo);
+            }
+            if($type==1){
+                $data['duration'] = $duration;
+            }else{
+                $data['duration'] = 0;
+            }
+            $m_forscreen = new \Common\Model\Smallapp\ForscreenRecordModel();
+            $m_forscreen->add($data);
+    
+            $m_public = new \Common\Model\Smallapp\PublicModel();
+            $res_public = $m_public->getOne('id',array('forscreen_id'=>$forscreen_id,'openid'=>$openid),'id desc');
+            if(empty($res_public)){
+                $public_data = array();
+                $public_data['forscreen_id'] = $forscreen_id;
+                $public_data['openid'] = $openid;
+                $public_data['box_mac']= $box_mac;
+                $public_data['public_text'] = $public_text;
+                if($action==4){
+                    $public_data['res_type'] = 1;
+                }else if($action==2 && $resource_type==2){
+                    $public_data['res_type'] = 2;
+                    $public_data['duration'] = $duration;
+                }
+                $public_data['res_url']   = $oss_addr;
+                $public_data['is_pub_hotelinfo'] =$is_pub_hotelinfo;
+                if($res_nums){
+                    $public_data['res_nums'] = $res_nums;
+                }
+                $public_data['status'] =1;
+                $m_public->add($public_data);
+            }
+            $pubdetail_data = array('forscreen_id'=>$forscreen_id,'resource_id'=>$resource_id,
+                'res_url'=>$oss_addr,'duration'=>$duration,'resource_size'=>$resource_size);
+            $m_publicdetail = new \Common\Model\Smallapp\PubdetailModel();
+            $m_publicdetail->add($pubdetail_data);
+        }else{
+            $redis = SavorRedis::getInstance();
+            $redis->select(5);
+            $cache_key = C('SAPP_SCRREN').":".$box_mac;
+            $redis->rpush($cache_key, json_encode($data));
+        }
+        $res = array('forscreen_id'=>$forscreen_id);
+        $this->to_back($res);
     }
 }

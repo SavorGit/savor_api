52800136cb2a5f9d780ca9ceaa077ba704da7901
zhangyingtao75144 <125929019@qq.com>
2020-08-28 13:47:03 +0800

    小程序互动页面banner

--- Application/Smallapp46/Controller/AdspositionController.class.php
+++ Application/Smallapp46/Controller/AdspositionController.class.php
@@ -39,12 +39,17 @@ class AdspositionController extends CommonController{
             $redis->select(1);
             $cache_key = 'smallapp:activity:kingmealhotel';
             $hotel_list = $redis->get($cache_key);
-            $hotel_list = json_decode($hotel_list);
+            $hotel_list = json_decode($hotel_list,true);
             $hotel_arr = [];
             foreach($hotel_list as $key=>$v){
                 $hotel_arr[] = $key;
             }
-            if(in_array($room_info['hotel_id'],$hotel_arr)){
+            
+            $start_time = $hotel_list[$room_info['hotel_id']][0]['start_time'];
+            $end_time = $hotel_list[$room_info['hotel_id']][0]['end_time'];
+            
+            $now_time = date('Y-m-d H:i:s');
+            if(in_array($room_info['hotel_id'],$hotel_arr)&& $now_time>=$start_time && $now_time<$end_time){
                 $info = [];
                 $info['appid'] = '';
                 $info['bindtap'] = 'gotoActivity';
@@ -64,42 +69,54 @@ class AdspositionController extends CommonController{
         
     }
     private function getAdList($position){
-        $fields = 'id,name,media_id,linkcontent,clicktype,appid,position,bindtap';
-        $where = array('status'=>1);
-        $orderby = 'sort desc,id desc';
-        $m_adsposition = new \Common\Model\Smallapp\AdspositionModel();
-        
-        $result = array();
-        if($position && strstr($position, ',')){
-        
-            $where['position'] = array('in',$position);
-            $res_positions = $m_adsposition->getDataList($fields,$where,$orderby);
-            if(!empty($res_positions)){
-                $m_media = new \Common\Model\MediaModel();
-                foreach ($res_positions as $k=>$v){
-                    $res_media = $m_media->getMediaInfoById($v['media_id']);
-                    $v['oss_addr'] = $res_media['oss_addr'];
-                    unset($v['media_id']);
-                    $result[$v['position']][] = $v;
+        $redis = SavorRedis::getInstance();
+        $redis->select(1);
+        $cache_key = 'smallapp:adsposition:'.$position;
+        $result = $redis->get($cache_key);
+        if(empty($result)){
+            $fields = 'id,name,media_id,linkcontent,clicktype,appid,position,bindtap';
+            $where = array('status'=>1);
+            $orderby = 'sort desc,id desc';
+            $m_adsposition = new \Common\Model\Smallapp\AdspositionModel();
+            
+            $result = array();
+            if($position && strstr($position, ',')){
+            
+                $where['position'] = array('in',$position);
+                $res_positions = $m_adsposition->getDataList($fields,$where,$orderby);
+                if(!empty($res_positions)){
+                    $m_media = new \Common\Model\MediaModel();
+                    foreach ($res_positions as $k=>$v){
+                        $res_media = $m_media->getMediaInfoById($v['media_id']);
+                        $v['oss_addr'] = $res_media['oss_addr'];
+                        unset($v['media_id']);
+                        $result[$v['position']][] = $v;
+                    }
                 }
+            }else {
+                $where['position'] = $position;
+                $order =" a.order desc";
+            
+                $res_positions = $m_adsposition->getDataList($fields,$where,$orderby);
+            
+                if(!empty($res_positions)){
+                    $m_media = new \Common\Model\MediaModel();
+                    foreach ($res_positions as $k=>$v){
+                        $res_media = $m_media->getMediaInfoById($v['media_id']);
+                        $v['oss_addr'] = $res_media['oss_addr'];
+                        unset($v['media_id']);
+                        $result[] = $v;
+                    }
+                }
+            
+            } 
+            if(!empty($result)){
+                $redis->set($cache_key, json_encode($result),14400);
             }
         }else {
-            $where['position'] = $position;
-            $order =" a.order desc";
-        
-            $res_positions = $m_adsposition->getDataList($fields,$where,$orderby);
-        
-            if(!empty($res_positions)){
-                $m_media = new \Common\Model\MediaModel();
-                foreach ($res_positions as $k=>$v){
-                    $res_media = $m_media->getMediaInfoById($v['media_id']);
-                    $v['oss_addr'] = $res_media['oss_addr'];
-                    unset($v['media_id']);
-                    $result[] = $v;
-                }
-            }
-        
+            $result = json_decode($result,true);
         }
+        
         return $result;
     }
 }

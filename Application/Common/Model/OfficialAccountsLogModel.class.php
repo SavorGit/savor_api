49444ea6e0178329706fc2e2aa3f85589fe384f5
liubin0125 <liubin-0125@163.com>
2020-12-24 16:12:39 +0800

    增加记录公众号日志

---
+++ Application/Common/Model/OfficialAccountsLogModel.class.php
@@ -0,0 +1,6 @@
+<?php
+namespace Common\Model;
+
+class OfficialAccountsLogModel extends BaseModel {
+	protected $tableName='official_accounts_log';
+}
--- Application/H5/Controller/WechatController.class.php
+++ Application/H5/Controller/WechatController.class.php
@@ -8,20 +8,25 @@ class WechatController extends Controller {
     public function message(){
         $params = file_get_contents('php://input');
         $log_file_name = APP_PATH.'Runtime/Logs/'.'wxmessage_'.date("Ymd").".log";
-        $log_content = date('Y-m-d H:i:s')."|content|$params \r\n";
-
+        $log_content = date('Y-m-d H:i:s')."|params|$params \r\n";
         @file_put_contents($log_file_name, $log_content, FILE_APPEND);
+
         $wx_config = C('WX_MP_CONFIG');
         $options = array('appid'=>$wx_config['appid'],'appsecret'=>$wx_config['appsecret'],'token'=>'2iRIcv5fJyHGonO3eNn4UNID0lpZAspo');
-
         $weObj = new WechatMessage($options);
         $weObj->valid();
         $revObj = $weObj->getRev();
         $type = $revObj->getRevType();
+
         $content = '欢迎关注';
+        $smallapp_config = C('SMALLAPP_CONFIG');
+        $appid = $smallapp_config['appid'];
         switch($type) {
             case WechatMessage::MSGTYPE_TEXT://回复或关键词
-                $content = '欢迎关注';
+                $page_url = "pages/index/index";
+
+                $content = "<a href='http://www.qq.com' data-miniprogram-appid='{$appid}' data-miniprogram-path='{$page_url}'>欢迎使用热点投屏，请点击此处进行投屏</a>";
+                $weObj->text($content)->reply();
                 /*
                 $content_name = $revObj->getRevContent();
                 $where = array('replyway'=>1);
@@ -39,18 +44,51 @@ class WechatController extends Controller {
             case WechatMessage::MSGTYPE_EVENT:
                 $eventinfo = $revObj->getRevEvent();
                 if($eventinfo['event'] == WechatMessage::EVENT_SUBSCRIBE || $eventinfo['event'] == WechatMessage::EVENT_SCAN) {//关注
-                }
-                if(!empty($eventinfo['key'])){
-                    $qrcode = $eventinfo['key'];
-                    $wx_mpopenid = $revObj->getRevFrom();
-                    $page_url = "pages/forscreen/forscreen?s={$qrcode}&wxmpopenid={$wx_mpopenid}";
-                    $content = "请点击此处<a href='http://www.qq.com' data-miniprogram-appid='wxfdf0346934bb672f' data-miniprogram-path='{$page_url}'>跳转热点投屏小程序</a>";
+                    if(!empty($eventinfo['key'])){
+                        if($eventinfo['event'] == WechatMessage::EVENT_SUBSCRIBE){
+                            $qrcode = str_replace('qrscene_','',$eventinfo['key']);//qrscene_mxxonmaEg
+                        }else{
+                            $qrcode = $eventinfo['key'];
+                        }
+                        $wx_mpopenid = $revObj->getRevFrom();
+                        $page_url = "pages/forscreen/forscreen?official={$qrcode}&wxmpopenid={$wx_mpopenid}&s={$qrcode}";
+                        $thumb_media_id = 's_KxN5aPbIS1vmNmCnJCpUwtzlzS0vOaibLW9Qs1O-w';
+                        $data = array(
+                            'touser'=>$wx_mpopenid,
+                            'msgtype'=>"miniprogrampage",
+                            'miniprogrampage'=>array(
+                                'title'=>'欢迎使用热点投屏',
+                                'appid'=>$appid,
+                                'pagepath'=>$page_url,
+                                'thumb_media_id'=>$thumb_media_id,
+                            ),
+                        );
+                        $wechat = new \Common\Lib\Wechat();
+                        $res = $wechat->customsend(json_encode($data,JSON_UNESCAPED_UNICODE));
+                        if(!empty($res)){
+                            $res_info = json_decode($res,true);
+                            if($res_info['errcode']!=0){
+                                $res = $wechat->customsend(json_encode($data,JSON_UNESCAPED_UNICODE));
+                            }
+                        }
+                        $log_content = date('Y-m-d H:i:s')."|openid|$wx_mpopenid|push_result|$res \r\n";
+                        @file_put_contents($log_file_name, $log_content, FILE_APPEND);
+                    }
                 }
                 break;
-
         }
-        $weObj->text($content)->reply();
-
+        $rev_data = $revObj->getRevData();
+        if(!empty($rev_data)){
+            $data = array('openid'=>$rev_data['FromUserName'],'msgtype'=>$rev_data['MsgType'],'event'=>$rev_data['Event'],
+                'event_content'=>$rev_data['EventKey']
+            );
+            if(!empty($rev_data['CreateTime'])){
+                $data['create_time'] = date('Y-m-d H:i:s',$rev_data['CreateTime']);
+            }
+            $m_official = new \Common\Model\OfficialAccountsLogModel();
+            $m_official->add($data);
+        }
+        echo 'success';
     }
 
 

b826a31df32afe1c4b1c38efbba93012ad13431a
liubin0125 <liubin-0125@163.com>
2021-02-07 21:09:01 +0800

    修改极简版阀值为5M

--- Application/Smallapp46/Controller/IndexController.class.php
+++ Application/Smallapp46/Controller/IndexController.class.php
@@ -383,7 +383,7 @@ class IndexController extends CommonController{
             $data['limit_video_size'] = 10485760;
             $data['tail_lenth']   = 1024*1024;
             $data['max_video_size'] = 1024*1024*300;
-            $data['max_user_forvideo_size'] = 1024*1024*20;
+            $data['max_user_forvideo_size'] = 1024*1024*5;
             $data['wifi_timeout_time'] = 20000;   //链接wifi超时时间
         }else{
             $data = array('is_have'=>0);

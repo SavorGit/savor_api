6fde250475fbd0725366c35739c424ecf081bb42
liubin0125 <liubin-0125@163.com>
2021-03-22 09:57:38 +0800

    修改发现页显示日期

--- Application/Smallapp46/Controller/FindController.class.php
+++ Application/Smallapp46/Controller/FindController.class.php
@@ -840,7 +840,7 @@ class FindController extends CommonController{
         $fields= 'a.forscreen_id,a.forscreen_char,a.public_text,a.res_type,a.res_nums,a.is_pub_hotelinfo,
                     a.create_time,hotel.name hotel_name,user.avatarUrl,user.nickName';
         $order = 'a.res_type desc,a.create_time desc';
-        $maps = array('a.forscreen_id'=>$forscreen_id);
+        $maps = array('a.forscreen_id'=>$forscreen_id,'box.state'=>1,'box.flag'=>0);
         $rec_pub_list = $m_public->getList($fields, $maps);
         $pub_info = $rec_pub_list[0];
         $oss_host = 'http://'. C('OSS_HOST').'/';
@@ -877,7 +877,13 @@ class FindController extends CommonController{
             if(empty($pub_info['nickName'])){
                 $pub_info['nickName'] = '游客';
             }
-            $create_time = viewTimes(strtotime($pub_info['create_time']));
+            $now = time();
+            $diff_time =  $now - strtotime($pub_info['create_time']);
+            if($diff_time<=86400){
+                $create_time = viewTimes(strtotime($pub_info['create_time']));
+            }else{
+                $create_time = '';
+            }
             //收藏个数
             $map = array('res_id'=>$forscreen_id,'type'=>2,'status'=>1);
             $collect_num = $m_collect->countNum($map);
@@ -1079,7 +1085,13 @@ class FindController extends CommonController{
                 }
             }
             $all_public[$key]['pubdetail'] = $pubdetail_info;
-            $all_public[$key]['create_time'] = viewTimes(strtotime($v['create_time']));
+            $now = time();
+            $diff_time =  $now - strtotime($v['create_time']);
+            if($diff_time<=86400){
+                $all_public[$key]['create_time'] = viewTimes(strtotime($v['create_time']));
+            }else{
+                $all_public[$key]['create_time'] = '';
+            }
 
             //获取是否收藏、分享个数、收藏个数、获取播放次数
             $rets = $this->getFindnums($openid,$v['forscreen_id'],2);

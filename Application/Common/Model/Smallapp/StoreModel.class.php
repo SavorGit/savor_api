c7e261d184bb0fbb742524f37e1bab66b752e696
liubin0125 <liubin-0125@163.com>
2021-03-25 11:15:03 +0800

    新增店铺列表

---
+++ Application/Common/Model/Smallapp/StoreModel.class.php
@@ -0,0 +1,82 @@
+<?php
+namespace Common\Model\Smallapp;
+use Common\Model\BaseModel;
+
+class StoreModel extends BaseModel {
+	protected $tableName='smallapp_store';
+
+    public function getAllStores($area_id=1){
+        $sql_hotel = $this->buildHotelSql($area_id);
+        $sql_store = $this->buildStoreSql($area_id);
+        $all_sql = "($sql_hotel) union ($sql_store) order by hotel_id desc";
+        $result = $this->query($all_sql);
+        return $result;
+    }
+
+    public function getHotelStore($area_id,$county_id=0,$food_style_id=0,$avg_id=0){
+        $sql_hotel = $this->buildHotelSql($area_id,$county_id,$food_style_id,$avg_id);
+        return $this->query($sql_hotel);
+    }
+
+    public function getLifeStore($area_id,$county_id=0,$cate_id=0,$avg_id=0){
+        $sql_store = $this->buildStoreSql($area_id,$county_id,$cate_id,$avg_id);
+        return $this->query($sql_store);
+    }
+
+    public function buildHotelSql($area_id,$county_id=0,$food_style_id=0,$avg_id=0){
+        $sql_hotel = "select hotel.id as hotel_id,hotel.media_id,hotel.name,hotel.addr,hotel.tel,hotel.gps,ext.avg_expense,food.name tag_name,'120' as cate_id
+            from savor_hotel as hotel left join savor_hotel_ext as ext on hotel.id=ext.hotel_id left join savor_hotel_food_style as food on ext.food_style_id=food.id
+            where hotel.area_id={$area_id} and hotel.state=1 and hotel.flag=0";
+        $hotel_box_types = C('HEART_HOTEL_BOX_TYPE');
+        $hotel_box_type_str = join(',',array_keys($hotel_box_types));
+        $sql_hotel.= " and hotel.hotel_box_type in ($hotel_box_type_str)";
+        $sql_hotel.= " and hotel.id not in (7,482,504,791,508,844,845,597,201,493,883,53)";
+
+        if($county_id){
+            $sql_hotel.=" and hotel.county_id={$county_id}";
+        }
+        if($food_style_id){
+            $sql_hotel.=" and ext.food_style_id=$food_style_id";
+        }
+        if($avg_id){
+            $all_avg = C('PERSON_PRICE');
+            $avg_info = $all_avg[$avg_id];
+            $sql_hotel.=" and ext.avg_expense>{$avg_info['min']} and ext.avg_expense<={$avg_info['max']}";
+        }
+        $sql_hotel.=" order by hotel_id desc";
+        return $sql_hotel;
+    }
+
+    public function buildStoreSql($area_id,$county_id=0,$cate_id=0,$avg_id=0){
+        $sql_store = "select store.id as hotel_id,store.cover_media_id as media_id,store.name,store.addr,store.tel,store.gps,store.avg_expense,category.name as tag_name,store.category_id as cate_id
+            from savor_smallapp_store as store left join savor_category as category on store.category_id=category.id 
+            where store.area_id={$area_id} and store.status=1";
+        if($county_id){
+            $sql_store.=" and store.county_id={$county_id}";
+        }
+        if($cate_id){
+            $m_category = new \Common\Model\Smallapp\CategoryModel();
+            $res_category = $m_category->getDataList('id',array('parent_id'=>$cate_id),'id asc');
+            if(!empty($res_category)){
+                $cate_ids = array();
+                foreach ($res_category as $v){
+                    $cate_ids[]=$v['id'];
+                }
+                $cate_str = join(',',$cate_ids);
+                $sql_store.=" and store.category_id in ({$cate_str})";
+            }else{
+                $sql_store.=" and store.category_id={$cate_id}";
+            }
+        }
+        if($avg_id){
+            $all_avg = C('PERSON_PRICE');
+            $avg_info = $all_avg[$avg_id];
+            $sql_store.=" and store.avg_expense>{$avg_info['min']} and store.avg_expense<={$avg_info['max']}";
+        }
+        $sql_store.=" order by hotel_id desc";
+
+        return $sql_store;
+    }
+
+
+}
---
+++ Application/Smallapp46/Controller/StoreController.class.php
@@ -0,0 +1,101 @@
+<?php
+namespace Smallapp46\Controller;
+use \Common\Controller\CommonController as CommonController;
+class StoreController extends CommonController{
+
+    /**
+     * 构造函数
+     */
+    function _init_() {
+        switch(ACTION_NAME) {
+            case 'dataList':
+                $this->is_verify =1;
+                $this->valid_fields = array('page'=>1001,'area_id'=>1001,'country_id'=>1002,
+                    'latitude'=>1002,'longitude'=>1002,'cate_id'=>1001,
+                    'food_style_id'=>1002,'avg_exp_id'=>1002
+                );
+                break;
+
+        }
+        parent::_init_();
+    }
+    /**
+     * @desc 店铺列表
+     */
+    public function dataList(){
+        $page     = $this->params['page'] ? $this->params['page'] :1;
+        $area_id  = $this->params['area_id'] ? $this->params['area_id'] :1;
+        $county_id = $this->params['county_id'];
+        $cate_id = intval($this->params['cate_id']);
+        $food_style_id = $this->params['food_style_id'];
+        $avg_id   = $this->params['avg_exp_id'];
+        $latitude = $this->params['latitude'];
+        $longitude = $this->params['longitude'];
+        $pagesize = 10;
+
+        $m_store = new \Common\Model\Smallapp\StoreModel();
+        if($cate_id==0){
+            $res_store = $m_store->getAllStores($area_id);
+        }elseif($cate_id==120){
+            $res_store = $m_store->getHotelStore($area_id,$county_id,$food_style_id,$avg_id);
+        }else{
+            $res_store = $m_store->getLifeStore($area_id,$county_id,$cate_id,$avg_id);
+        }
+        if($longitude>0 && $latitude>0){
+            $bd_lnglat = getgeoByTc($latitude, $longitude);
+            foreach($res_store as $key=>$v){
+                $res_store[$key]['dis'] = '';
+                if($v['gps']!='' && $longitude>0 && $latitude>0){
+                    $latitude = $bd_lnglat[0]['y'];
+                    $longitude = $bd_lnglat[0]['x'];
+
+                    $gps_arr = explode(',',$v['gps']);
+                    $dis = geo_distance($latitude,$longitude,$gps_arr[1],$gps_arr[0]);
+                    $res_store[$key]['dis_com'] = $dis;
+                    if($dis>1000){
+                        $tmp_dis = $dis/1000;
+                        $dis = sprintf('%0.2f',$tmp_dis);
+                        $dis = $dis.'km';
+                    }else{
+                        $dis = intval($dis);
+                        $dis = $dis.'m';
+                    }
+                    $res_store[$key]['dis'] = $dis;
+                }else {
+                    $res_store[$key]['dis'] = '';
+                }
+            }
+            sortArrByOneField($res_store,'dis_com');
+        }
+
+        $offset = $page * $pagesize;
+        $hotel_list = array_slice($res_store,0,$offset);
+        $m_meida = new \Common\Model\MediaModel();
+        $datalist = array();
+        foreach ($hotel_list as $k=>$v){
+            $tag_name = $v['tag_name'];
+            if(empty($tag_name)){
+                $tag_name = '';
+            }
+            if($v['media_id']){
+                $res_media = $m_meida->getMediaInfoById($v['media_id']);
+                $img_url = $res_media['oss_addr'].'?x-oss-process=image/resize,p_20';
+                $ori_img_url = $res_media['oss_addr'];
+            }else{
+                $img_url = 'http://oss.littlehotspot.com/media/resource/kS3MPQBs7Y.png';
+                $ori_img_url = $img_url;
+            }
+            $dis = $v['dis'];
+            if(empty($dis)){
+                $dis = '';
+            }
+            $datalist[]=array('hotel_id'=>$v['hotel_id'],'name'=>$v['name'],'addr'=>$v['addr'],'tel'=>$v['tel'],'avg_expense'=>$v['avg_expense'],
+                'dis'=>$dis,'tag_name'=>$tag_name,'img_url'=>$img_url,'ori_img_url'=>$ori_img_url
+            );
+
+        }
+        $this->to_back(array('datalist'=>$datalist));
+    }
+
+
+}

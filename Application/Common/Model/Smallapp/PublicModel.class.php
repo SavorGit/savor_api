c9410255b8abae1ef2ada7271d5f95165c8eb68d
liubin0125 <liubin-0125@163.com>
2021-08-13 10:55:46 +0800

    新增发现页图片和视频混合显示列表

--- Application/Common/Model/Smallapp/PublicModel.class.php
+++ Application/Common/Model/Smallapp/PublicModel.class.php
@@ -67,4 +67,28 @@ class PublicModel extends Model
 	         ->count();
 	    return $nums;
 	}
+
+    public function getFindnums($openid,$res_id,$type){
+        $m_collect = new \Common\Model\Smallapp\CollectModel();
+        $m_share   = new \Common\Model\Smallapp\ShareModel();
+        $m_collect_count = new \Common\Model\Smallapp\CollectCountModel();
+
+        $map = array('openid'=>$openid,'res_id'=>$res_id,'type'=>$type,'status'=>1);
+        $is_collect = $m_collect->countNum($map);
+        if(empty($is_collect)){
+            $is_collect = 0;
+        }else {
+            $is_collect = 1;
+        }
+        $map = array('res_id'=>$res_id,'type'=>$type,'status'=>1);
+        $collect_num = $m_collect->countNum($map);
+        $ret = $m_collect_count->field('nums')->where(array('res_id'=>$res_id))->find();
+        $collect_num = $collect_num+intval($ret['nums']);
+        //分享个数
+        $map = array('res_id'=>$res_id,'type'=>$type,'status'=>1);
+        $share_num = $m_share->countNum($map);
+
+        $data = array('is_collect'=>$is_collect,'collect_num'=>$collect_num,'share_num'=>$share_num);
+        return $data;
+    }
 }

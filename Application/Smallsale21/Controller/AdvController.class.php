ce200994d7f0d8e81bec9fb124016e681eaa5782
liubin0125 <liubin-0125@163.com>
2022-05-30 10:09:40 +0800

    修改本店有售广告封面图

--- Application/Smallsale21/Controller/AdvController.class.php
+++ Application/Smallsale21/Controller/AdvController.class.php
@@ -64,7 +64,7 @@ class AdvController extends CommonController{
             $now_date = date('Y-m-d H:i:s');
             $m_life_adshotel = new \Common\Model\Smallapp\StoresaleAdsHotelModel();
             $fields = "ads.id,ads.name as title,ads.img_url,ads.create_time,media.id as media_id,media.oss_addr,
-            media.duration as duration,media.oss_filesize as resource_size,sads.goods_id,dg.finance_goods_id";
+            media.duration as duration,media.oss_filesize as resource_size,sads.goods_id,dg.finance_goods_id,sads.cover_img_media_id";
             $where = array('a.hotel_id'=>$hotel_id);
             $where['sads.start_date'] = array('ELT',$now_date);
             $where['sads.end_date'] = array('EGT',$now_date);
@@ -73,6 +73,7 @@ class AdvController extends CommonController{
             $res_data = $m_life_adshotel->getGoodsList($fields, $where, $order);
             $win_list = array();
             if(!empty($res_data)){
+                $m_media = new \Common\Model\MediaModel();
                 foreach($res_data as $v){
                     if(in_array($v['finance_goods_id'],$hotel_stock[$hotel_id]['goods_ids'])){
                         $create_time = $v['create_time'];
@@ -87,7 +88,11 @@ class AdvController extends CommonController{
                         $oss_info = pathinfo($forscreen_url);
                         $pdetail['filename'] = $oss_info['basename'];
 
-                        $img_url = $v['img_url']? $v['img_url'] :'media/resource/EDBAEDArdh.png';
+                        $img_url = 'media/resource/EDBAEDArdh.png';
+                        if($v['cover_img_media_id']){
+                            $res_media = $m_media->getMediaInfoById($v['cover_img_media_id']);
+                            $img_url = $res_media['oss_path'];
+                        }
                         $pdetail['img_url'] = $oss_host.$img_url;
                         $dinfo['pubdetail'] = array($pdetail);
                         $win_list[] = $dinfo;
@@ -100,94 +105,6 @@ class AdvController extends CommonController{
         $data = array('datalist'=>$ads_list);
         $this->to_back($data);
     }
-	public function getResList(){
-		$hotel_id = intval($this->params['hotel_id']);
-        $page = intval($this->params['page']);
-        $pagesize = 100;
-        $oss_host = 'http://'. C('OSS_HOST').'/';
-
-        $m_ads = new \Common\Model\AdsModel();
-        $where = array('a.hotel_id'=>$hotel_id,'a.state'=>1,'a.is_online'=>1,'a.type'=>3);
-        $fields = 'a.id,a.name title,a.img_url,a.duration,a.create_time,b.id as media_id,b.oss_addr,b.oss_filesize as resource_size';
-        $all_nums = $page * $pagesize;
-        $limit = "0,$all_nums";
-        $res_ads = $m_ads->getAdsList($fields,$where,'a.id desc',$limit,'a.media_id');
-        $ads_list = array();
-        if(!empty($res_ads)){
-            foreach($res_ads as $v){
-                $create_time = $v['create_time'];
-                $dinfo = array('id'=>$v['id'],'title'=>$v['title'],'forscreen_id'=>0,'res_type'=>2,'res_nums'=>1,'create_time'=>$create_time);
-
-                $res_url = $oss_host.$v['oss_addr'];
-                $forscreen_url = $v['oss_addr'];
-                $duration = intval($v['duration']);
-                $resource_size = $v['resource_size'];
-                $res_id = $v['media_id'];
-                $pdetail = array('res_url'=>$res_url,'forscreen_url'=>$forscreen_url,'duration'=>$duration,
-                    'resource_size'=>$resource_size,'res_id'=>$res_id);
-                $oss_info = pathinfo($forscreen_url);
-                $pdetail['filename'] = $oss_info['basename'];
-
-                $img_url = $v['img_url']? $v['img_url'] :'media/resource/EDBAEDArdh.png';
-                $pdetail['img_url'] = $oss_host.$img_url;
-                $dinfo['pubdetail'] = array($pdetail);
-                $ads_list[] = $dinfo;
-            }
-        }
-		
-		//酒水
-		$redis = SavorRedis::getInstance();
-        $redis->select(9);
-        $cache_key = C('FINANCE_HOTELSTOCK');
-		$result  = $redis->get($cache_key);
-		
-		$hotel_arr = json_decode($result,true);
-		$win_list = array();
-		if(!empty($hotel_arr[$hotel_id])){
-		    $m_media = new \Common\Model\MediaModel();
-			$now_date = date('Y-m-d H:i:s');
-			$m_life_adshotel = new \Common\Model\Smallapp\StoresaleAdsHotelModel();
-			$fields = "media.id as vid,ads.id as ads_id,ads.is_sapp_qrcode,media.md5,ads.name as title,media.oss_addr as oss_path,media.duration as duration,
-					   media.surfix as suffix,sads.start_date,sads.end_date,sads.is_price,sads.goods_id,ads.resource_type as media_type,ads.create_time,
-					   media.oss_filesize as resource_size,media.id as media_id,sads.cover_img_media_id";
-			$where = array('a.hotel_id'=>$hotel_id);
-			$where['sads.start_date'] = array('ELT',$now_date);
-			$where['sads.end_date'] = array('EGT',$now_date);
-			$where['sads.state']= 1;
-			$order = "sads.id asc";
-			$res_data = $m_life_adshotel->getGoodsList($fields, $where, $order);
-			if(!empty($res_data)){
-				foreach($res_data as $v){
-					$dinfo = array('id'=>$v['ads_id'],'title'=>$v['title'],'forscreen_id'=>0,'res_type'=>2,'res_nums'=>1,'create_time'=>$create_time);
-					$res_url = $oss_host.$v['oss_path'];
-					$forscreen_url = $v['oss_path'];
-					$duration = intval($v['duration']);
-					$resource_size = $v['resource_size'];
-					$res_id = $v['media_id'];
-					$pdetail = array('res_url'=>$res_url,'forscreen_url'=>$forscreen_url,'duration'=>$duration,
-						'resource_size'=>$resource_size,'res_id'=>$res_id);
-					$oss_info = pathinfo($forscreen_url);
-					$pdetail['filename'] = $oss_info['basename'];
-
-                    $img_url = 'media/resource/EDBAEDArdh.png';
-                    if($v['cover_img_media_id']){
-                        $res_media = $m_media->getMediaInfoById($v['cover_img_media_id']);
-                        $img_url = $res_media['oss_path'];
-                    }
-					$pdetail['img_url'] = $oss_host.$img_url;
-					$dinfo['pubdetail'] = array($pdetail);
-					$win_list[] = $dinfo;
-				}
-			}
-		}
-		
-		
-		$result = array_merge($ads_list,$win_list);
-		
-        $data = array('datalist'=>$result);
-        $this->to_back($data);
-	}
-
 
 
 

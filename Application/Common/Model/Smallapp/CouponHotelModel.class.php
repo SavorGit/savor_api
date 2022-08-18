f7a964ca1efddfd697a703902dbc37ac4f1fdf23
liubin0125 <liubin-0125@163.com>
2022-08-18 16:36:50 +0800

    增加优惠券酒楼相关功能

---
+++ Application/Common/Model/Smallapp/CouponHotelModel.class.php
@@ -0,0 +1,7 @@
+<?php
+namespace Common\Model\Smallapp;
+use Common\Model\BaseModel;
+
+class CouponHotelModel extends BaseModel{
+	protected $tableName='smallapp_coupon_hotel';
+}
--- Application/Smallapp46/Controller/CouponController.class.php
+++ Application/Smallapp46/Controller/CouponController.class.php
@@ -24,6 +24,10 @@ class CouponController extends CommonController{
                 $this->is_verify = 1;
                 $this->valid_fields = array('openid'=>1001,'type'=>1001,'status'=>1001);
                 break;
+            case 'hotels':
+                $this->is_verify = 1;
+                $this->valid_fields = array('openid'=>1001,'coupon_id'=>1001);
+                break;
         }
         parent::_init_();
     }
@@ -114,7 +118,12 @@ class CouponController extends CommonController{
             $m_hotel = new \Common\Model\HotelModel();
             $nowtime = date('Y-m-d H:i:s');
             foreach ($res_coupon as $v){
-                $res_hotel = $m_hotel->getInfoById($v['hotel_id'],'name');
+                if($v['hotel_id']){
+                    $res_hotel = $m_hotel->getInfoById($v['hotel_id'],'name');
+                    $hotel_name = $res_hotel['name'];
+                }else{
+                    $hotel_name = '多餐厅可用';
+                }
                 $expire_time = date('Y.m.d H:i',strtotime($v['end_time']));
                 $start_time = date('Y.m.d H:i',strtotime($v['start_time']));
                 if($v['min_price']>0){
@@ -139,7 +148,7 @@ class CouponController extends CommonController{
                     }
                 }
                 $info = array('coupon_user_id'=>$v['id'],'money'=>$v['money'],'min_price'=>$min_price,'expire_time'=>"有效期至{$expire_time}",
-                    'hotel_name'=>$res_hotel['name'],'range_str'=>$range_str,'range_goods'=>$range_goods,'start_time'=>$start_time,'end_time'=>$expire_time
+                    'hotel_name'=>$hotel_name,'range_str'=>$range_str,'range_goods'=>$range_goods,'start_time'=>$start_time,'end_time'=>$expire_time
                 );
 
                 switch ($v['ustatus']){
@@ -181,6 +190,20 @@ class CouponController extends CommonController{
         $res_data = array();
         if(!empty($res_coupon)){
             $coupon_info = $res_coupon[0];
+
+            $res_coupon_hotel = array();
+            if($coupon_info['hotel_id']>0){
+                $hotel_num = 1;
+                $m_hotel = new \Common\Model\HotelModel();
+                $res_hotel = $m_hotel->getInfoById($coupon_info['hotel_id'],'name');
+                $hotel_name = $res_hotel['name'];
+            }else{
+                $hotel_name = '多餐厅可用';
+                $m_coupon_hotel = new \Common\Model\Smallapp\CouponHotelModel();
+                $res_coupon_hotel = $m_coupon_hotel->getDataList('hotel_id',array('coupon_id'=>$coupon_info['coupon_id']),'id desc');
+                $hotel_num = count($res_coupon_hotel);
+            }
+
             $range_goods = array();
             if($coupon_info['use_range']==1){
                 $range_str = '全部活动酒水';
@@ -188,7 +211,11 @@ class CouponController extends CommonController{
                 $range_str = '部分活动酒水';
                 $range_finance_goods_ids = explode(',',trim($coupon_info['range_finance_goods_ids'],','));
                 $m_hotelgoods = new \Common\Model\Smallapp\HotelgoodsModel();
-                $res_data = $m_hotelgoods->getStockGoodsList($coupon_info['hotel_id'],0,1000);
+                if($coupon_info['hotel_id']>0){
+                    $res_data = $m_hotelgoods->getStockGoodsList($coupon_info['hotel_id'],0,1000);
+                }else{
+                    $res_data = $m_hotelgoods->getALLhotelStockGoodsList($res_coupon_hotel);
+                }
                 if(!empty($res_data)){
                     foreach ($res_data as $v){
                         if(in_array($v['finance_goods_id'],$range_finance_goods_ids)){
@@ -197,12 +224,9 @@ class CouponController extends CommonController{
                     }
                 }
             }
-            $m_hotel = new \Common\Model\HotelModel();
-            $res_hotel = $m_hotel->getInfoById($coupon_info['hotel_id'],'name');
-            $host_name = 'https://'.$_SERVER['HTTP_HOST'];
-
             $en_data = array('type'=>'coupon','id'=>$coupon_id);
             $data_id = encrypt_data(json_encode($en_data));
+            $host_name = 'https://'.$_SERVER['HTTP_HOST'];
             $qrcode_url = $host_name."/smallapp46/qrcode/getCouponQrcode?data_id={$data_id}";
             if($coupon_info['min_price']>0){
                 $min_price = "满{$coupon_info['min_price']}可用";
@@ -215,11 +239,75 @@ class CouponController extends CommonController{
             $end_time = "至{$end_time}";
 
             $res_data = array('openid'=>$openid,'coupon_id'=>$coupon_id,'money'=>$coupon_info['money'],'use_range'=>$coupon_info['use_range'],
-                'range_str'=>$range_str,'min_price'=>$min_price,'start_time'=>$start_time,'end_time'=>$end_time,
-                'range_goods'=>$range_goods,'hotel_name'=>$res_hotel['name'],'qrcode_url'=>$qrcode_url
+                'range_str'=>$range_str,'min_price'=>$min_price,'start_time'=>$start_time,'end_time'=>$end_time,'hotel_num'=>$hotel_num,
+                'range_goods'=>$range_goods,'hotel_name'=>$hotel_name,'qrcode_url'=>$qrcode_url
             );
         }
         $this->to_back($res_data);
+    }
+
+    public function hotels(){
+        $openid = $this->params['openid'];
+        $coupon_id = intval($this->params['coupon_id']);
+        $page = $this->params['page'];
+        $pagesize = empty($this->params['pagesize'])?10:$this->params['pagesize'];
+
+        $m_user = new \Common\Model\Smallapp\UserModel();
+        $where = array('openid'=>$openid,'status'=>1);
+        $user_info = $m_user->getOne('id,openid,mpopenid',$where,'');
+        if(empty($user_info)){
+            $this->to_back(90157);
+        }
+        $m_coupon = new \Common\Model\Smallapp\UserCouponModel();
+        $where = array('a.id'=>$coupon_id);
+        $fields = 'a.*,coupon.use_range,coupon.range_finance_goods_ids';
+        $res_coupon = $m_coupon->getUsercouponDatas($fields,$where,'a.id desc','');
+        $coupon_info = $res_coupon[0];
+
+        $m_coupon_hotel = new \Common\Model\Smallapp\CouponHotelModel();
+        $res_coupon_hotel = $m_coupon_hotel->getDataList('hotel_id',array('coupon_id'=>$coupon_info['coupon_id']),'id desc');
+        $redis = new \Common\Lib\SavorRedis();
+        $redis->select(9);
+        $key = C('FINANCE_HOTELSTOCK');
+        $res_cache = $redis->get($key);
+        $hotel_stock = json_decode($res_cache,true);
+        $now_hotel_ids = array();
+        foreach ($res_coupon_hotel as $v){
+            if(isset($hotel_stock[$v['hotel_id']])){
+                $now_hotel_ids[]=$v['hotel_id'];
+            }
+        }
+        $data_list = array();
+        if(!empty($now_hotel_ids)){
+            $range_finance_goods_ids = explode(',',trim($coupon_info['range_finance_goods_ids'],','));
+            $offset = ($page-1) * $pagesize;
+            $m_hotel = new \Common\Model\HotelModel();
+            $where = array('id'=>array('in',$now_hotel_ids));
+            $res_hotel = $m_hotel->getHotelList($where,'pinyin asc',"$offset,$pagesize",'id,name');
+            $m_hotelgoods = new \Common\Model\Smallapp\HotelgoodsModel();
+            foreach ($res_hotel as $v){
+                $hotel_id = $v['id'];
+
+                $range_goods = array();
+                if($coupon_info['use_range']==1){
+                    $range_str = '全部活动酒水';
+                }else{
+                    $range_str = '部分活动酒水';
+                    $fields = 'g.id,g.name,g.price,g.finance_goods_id';
+                    $where = array('h.hotel_id'=>$v['id'],'g.type'=>43,'g.status'=>1);
+                    $order = 'g.id desc';
+                    $res_goods = $m_hotelgoods->getGoodsList($fields,$where,$order,'','');
+                    foreach ($res_goods as $gv){
+                        if(in_array($gv['finance_goods_id'],$range_finance_goods_ids) && in_array($gv['finance_goods_id'],$hotel_stock[$hotel_id]['goods_ids'])){
+                            $range_goods[]=$gv['name'];
+                        }
+                    }
+                }
+                $info = array('hotel_id'=>$hotel_id,'hotel_name'=>$v['name'],'range_str'=>$range_str,'range_goods'=>$range_goods);
+                $data_list[]=$info;
+            }
+        }
+        $this->to_back($data_list);
     }
 
 

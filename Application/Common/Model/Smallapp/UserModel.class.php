82bad391fd33a360cec65281182ec2d0f262318e
liubin0125 <liubin-0125@163.com>
2022-11-01 10:31:12 +0800

    增加获取会员弹框数据方法

--- Application/Common/Model/Smallapp/UserModel.class.php
+++ Application/Common/Model/Smallapp/UserModel.class.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * @desc 小程序用户
- */
 namespace Common\Model\Smallapp;
 use Think\Model;
 
-class UserModel extends Model
-{
+class UserModel extends Model{
 	protected $tableName='smallapp_user';
 	
 	public function addInfo($data,$type=1){
@@ -34,4 +30,48 @@ class UserModel extends Model
 	    $nums = $this->where($where)->count();
 	    return $nums;
 	}
+
+	public function getMemberPopupinfo($openid,$hotel_id=0,$room_id=0,$box_id=0){
+        $where = array('openid'=>$openid,'status'=>1);
+        $user_info = $this->getOne('id,openid,avatarUrl,nickName,mpopenid,vip_level,mobile,is_wx_auth', $where, '');
+
+        $vip_level = 0;
+        $is_wx_auth = 0;
+        $mobile = '';
+        if(!empty($user_info)){
+            $vip_level = $user_info['vip_level'];
+            $mobile = $user_info['mobile'];
+            $is_wx_auth = $user_info['is_wx_auth'];
+        }
+        $coupon_money = 0;
+        $coupon_end_time = '';
+        $coupon_unnum = 0;
+        if($vip_level==0){
+            $m_sys_config = new \Common\Model\SysConfigModel();
+            $sys_info = $m_sys_config->getAllconfig();
+            $vip_coupons = json_decode($sys_info['vip_coupons'],true);
+            $now_vip_level = 1;
+            if(!empty($vip_coupons) && !empty($vip_coupons[$now_vip_level])){
+                $m_coupon = new \Common\Model\Smallapp\CouponModel();
+                $res_all_coupon = $m_coupon->getALLDataList('*',array('id'=>array('in',$vip_coupons[$now_vip_level])),'end_time desc','','');
+                $end_time = date('Y年m月d日',strtotime($res_all_coupon[0]['end_time']));
+                $coupon_end_time = $end_time.'到期';
+                foreach ($res_all_coupon as $v){
+                    $coupon_money+=$v['money'];
+                }
+            }
+        }else{
+            $where = array('a.openid'=>$openid,'a.ustatus'=>1,'a.status'=>1,'coupon.type'=>2);
+            $fields = 'count(a.id) as num';
+            $m_coupon_user = new \Common\Model\Smallapp\UserCouponModel();
+            $res_coupon = $m_coupon_user->getUsercouponDatas($fields,$where,'a.id desc','');
+            if(!empty($res_coupon)){
+                $coupon_unnum = intval($res_coupon[0]['num']);
+            }
+        }
+        $data = array('openid'=>$openid,'vip_level'=>$vip_level,'coupon_money'=>$coupon_money,'coupon_end_time'=>$coupon_end_time,
+            'coupon_unnum'=>$coupon_unnum,'mobile'=>$mobile,'is_wx_auth'=>$is_wx_auth,
+            'hotel_id'=>$hotel_id,'room_id'=>$hotel_id,'box_id'=>$box_id,'code_msg'=>'');
+        return $data;
+    }
 }

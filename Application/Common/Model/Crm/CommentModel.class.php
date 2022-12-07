df996b808f79f83ca69751fe913e224f6b30b51c
liubin0125 <liubin-0125@163.com>
2022-12-07 09:59:10 +0800

    增加评论相关

---
+++ Application/Common/Model/Crm/CommentModel.class.php
@@ -0,0 +1,7 @@
+<?php
+namespace Common\Model\Crm;
+use Common\Model\BaseModel;
+
+class CommentModel extends BaseModel{
+	protected $tableName='crm_comment';
+}
--- Application/Common/Model/Crm/SalerecordRemindModel.class.php
+++ Application/Common/Model/Crm/SalerecordRemindModel.class.php
@@ -5,15 +5,17 @@ use Common\Model\BaseModel;
 class SalerecordRemindModel extends BaseModel{
 	protected $tableName='crm_salerecord_remind';
 
-    public function getList($fields,$where,$orderby,$limit=''){
+    public function getList($fields,$where,$orderby,$limit='',$group=''){
         $data = $this->alias('a')
             ->field($fields)
+            ->join('savor_crm_salerecord record on a.salerecord_id=record.id','left')
             ->join('savor_ops_staff staff on a.remind_user_id=staff.id','left')
             ->join('savor_smallapp_user user on staff.openid=user.openid','left')
             ->join('savor_sysuser sysuser on staff.sysuser_id=sysuser.id','left')
             ->where($where)
             ->order($orderby)
             ->limit($limit)
+            ->group($group)
             ->select();
         return $data;
     }

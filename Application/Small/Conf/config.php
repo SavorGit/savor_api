da69485111570838e68f41f58d86dd6a982fe9f2
zhangyingtao75144 <125929019@qq.com>
2023-01-18 13:32:35 +0800

    增加小平台接口 route_url

--- Application/Small/Conf/config.php
+++ Application/Small/Conf/config.php
@@ -2,6 +2,19 @@
 //系统配置
 $route_rules = array(
     '/^rds\/(\S{0,10})$/'=>'Jump/index?id=:1',
+    '/^small\/api\/getDownloadList\/(\d{0,10})\/(ads|adv|pro|vod|logo|load)$/'=>'small/api/getDownloadList?hotelid=:1&type=:2',
+    
+    
+    '/^small\/api\/getHotel\/(\d{0,10})$/'=>'small/api/getHotel?hotelid=:1',
+    
+    '/^small\/api\/getHotel\/(\d{0,10})\/v2$/'=>'small/api/getHotelvb?hotelid=:1',
+    
+    '/^small\/api\/getRoom\/(\d{0,10})$/'=>'small/api/getHotelRoom?hotelid=:1',
+    
+    '/^small\/api\/getSetTopBox\/(\d{0,10})$/'=>'small/api/getHotelBox?hotelid=:1',
+    '/^small\/api\/getTelevision\/(\d{0,10})$/'=>'small/api/getHotelTv?hotelid=:1',
+    '/^small\/api\/getUpgradeVersion\/(\d{0,10})\/(wwar|apk)$/' =>'small/api/getUpgradeVersion?hotelId=:1&type=:2',
+    
 );
 $config = array(
     'SHOW_PAGE_TRACE'=>false,

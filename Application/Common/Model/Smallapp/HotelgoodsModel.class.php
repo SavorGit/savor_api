06c9c3640117380a10c1e0865b287fefe0a31552
liubin0125 <liubin-0125@163.com>
2023-03-03 09:45:05 +0800

    优化酒楼库存缓存查询

--- Application/Common/Model/Smallapp/HotelgoodsModel.class.php
+++ Application/Common/Model/Smallapp/HotelgoodsModel.class.php
@@ -33,25 +33,21 @@ class HotelgoodsModel extends BaseModel{
     public function getStockGoodsList($hotel_id,$offset,$pagesize){
         $redis = new \Common\Lib\SavorRedis();
         $redis->select(9);
-        $key = C('FINANCE_HOTELSTOCK');
+        $key = C('FINANCE_HOTELSTOCK').':'.$hotel_id;
         $res_cache = $redis->get($key);
-        if(!empty($res_cache)){
+        if(!empty($res_cache)) {
             $hotel_stock = json_decode($res_cache,true);
-            if(isset($hotel_stock[$hotel_id])){
-                $fields = 'g.id,g.name,g.price,g.advright_media_id,g.small_media_id,g.cover_imgs,g.line_price,g.type,g.finance_goods_id';
-                $where = array('h.hotel_id'=>$hotel_id,'g.type'=>43,'g.status'=>1);
-                $order = 'g.wine_type asc';
-                $res_data = $this->getGoodsList($fields,$where,$order,'','');
-                $all_data = array();
-                foreach ($res_data as $v){
-                    if(in_array($v['finance_goods_id'],$hotel_stock[$hotel_id]['goods_ids'])){
-                        $all_data[]=$v;
-                    }
+            $fields = 'g.id,g.name,g.price,g.advright_media_id,g.small_media_id,g.cover_imgs,g.line_price,g.type,g.finance_goods_id';
+            $where = array('h.hotel_id'=>$hotel_id,'g.type'=>43,'g.status'=>1);
+            $order = 'g.wine_type asc';
+            $res_data = $this->getGoodsList($fields,$where,$order,'','');
+            $all_data = array();
+            foreach ($res_data as $v){
+                if(in_array($v['finance_goods_id'],$hotel_stock['goods_ids'])){
+                    $all_data[]=$v;
                 }
-                $data = array_slice($all_data,$offset,$pagesize);
-            }else{
-                $data = array();
             }
+            $data = array_slice($all_data,$offset,$pagesize);
         }else{
             $data = array();
         }
@@ -62,25 +58,27 @@ class HotelgoodsModel extends BaseModel{
         $redis = new \Common\Lib\SavorRedis();
         $redis->select(9);
         $key = C('FINANCE_HOTELSTOCK');
-        $res_cache = $redis->get($key);
-        $all_data = array();
-        if(!empty($res_cache)){
-            $hotel_stock = json_decode($res_cache,true);
-            $now_hotel_ids = array();
-            foreach ($hotel_ids as $v){
-                if(isset($hotel_stock[$v['hotel_id']])){
-                    $now_hotel_ids[]=$v['hotel_id'];
-                }
+
+        $now_hotel_ids = array();
+        $hotel_stock = array();
+        foreach ($hotel_ids as $v){
+            $hotel_key = $key.':'.$v['hotel_id'];
+            $res_cache = $redis->get($hotel_key);
+            if(!empty($res_cache)){
+                $hotel_cache = json_decode($res_cache,true);
+                $now_hotel_ids[]=$v['hotel_id'];
+                $hotel_stock[$v['hotel_id']]=$hotel_cache;
             }
-            $fields = 'g.id,g.name,g.price,g.advright_media_id,g.cover_imgs,g.line_price,g.type,g.finance_goods_id,h.hotel_id';
-            $where = array('h.hotel_id'=>array('in',$now_hotel_ids),'g.type'=>43,'g.status'=>1);
-            $order = 'h.hotel_id asc';
-            $res_data = $this->getGoodsList($fields,$where,$order,'','');
-            foreach ($res_data as $v){
-                $hotel_id = $v['hotel_id'];
-                if(in_array($v['finance_goods_id'],$hotel_stock[$hotel_id]['goods_ids'])){
-                    $all_data[$v['finance_goods_id']]=$v;
-                }
+        }
+        $fields = 'g.id,g.name,g.price,g.advright_media_id,g.cover_imgs,g.line_price,g.type,g.finance_goods_id,h.hotel_id';
+        $where = array('h.hotel_id'=>array('in',$now_hotel_ids),'g.type'=>43,'g.status'=>1);
+        $order = 'h.hotel_id asc';
+        $res_data = $this->getGoodsList($fields,$where,$order,'','');
+        $all_data = array();
+        foreach ($res_data as $v){
+            $hotel_id = $v['hotel_id'];
+            if(in_array($v['finance_goods_id'],$hotel_stock[$hotel_id]['goods_ids'])){
+                $all_data[$v['finance_goods_id']]=$v;
             }
         }
         return $all_data;

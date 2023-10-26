22c57e3d97a41b45b7143983c6e64da2f361dae3
liubin0125 <liubin-0125@163.com>
2023-10-26 14:22:09 +0800

    优化代码

--- Application/Common/Model/Smallapp/OrdersettlementModel.class.php
+++ Application/Common/Model/Smallapp/OrdersettlementModel.class.php
@@ -9,6 +9,7 @@ class OrdersettlementModel extends BaseModel{
         $data = $this->alias('a')
             ->field($fields)
             ->join('savor_smallapp_distribution_user as duser on a.distribution_user_id=duser.id','left')
+            ->join('savor_smallapp_user as user on a.openid=user.openid','left')
             ->where($where)
             ->select();
         return $data;

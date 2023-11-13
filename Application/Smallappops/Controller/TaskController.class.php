32b3fe05fcdd56154dca138d39472765df637f0a
liubin0125 <liubin-0125@163.com>
2023-11-13 14:12:59 +0800

    优化任务酒楼列表关联是否为售酒餐厅

--- Application/Smallappops/Controller/TaskController.class.php
+++ Application/Smallappops/Controller/TaskController.class.php
@@ -219,7 +219,7 @@ class TaskController extends CommonController{
         }
         $hotel_role_type = $res_staff['hotel_role_type'];//酒楼角色类型1全国,2城市,3个人,4城市和个人,5全国财务,6城市财务
         $permission = json_decode($res_staff['permission'],true);
-        $where = array('a.off_state'=>1,'a.status'=>array('in','0,1'),'task.type'=>array('neq',11));
+        $where = array('a.off_state'=>1,'ext.is_salehotel'=>1,'a.status'=>array('in','0,1'),'task.type'=>array('neq',11));
         if($task_id>0){
             $where['a.task_id'] = $task_id;
         }
